b2a6f631946fa43a1987f9d5947b64943ceee018
Cristian Angel Puma Villalva <cristian.puma.es6@gmail.com>
2021-02-02 17:04:44 -0500

    add routing login

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+// Components CRUD
+import { SingUpComponent } from './components/sing-up/sing-up.component';
+
+const routes: Routes = [
+  { path: '', component: SingUpComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
