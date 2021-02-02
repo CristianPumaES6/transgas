01dd4501a36406f6a95003ffafb5e09e9e8fd6ce
Cristian Angel Puma Villalva <cristian.puma.es6@gmail.com>
2021-02-01 20:14:14 -0500

    Configuracion sqlite

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,8 +2,24 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+// TypeOrm
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+// Others
+import { join } from 'path';
+
+// Path
+import { SQLITE_PATH } from './config/path.config';
+
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: join(SQLITE_PATH, 'dbTransgas.sqlite3'),
+      entities: [join(__dirname, '**/**.entity{.ts,.js}')],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ backend/src/config/path.config.ts
@@ -0,0 +1,5 @@
+import * as path from 'path';
+
+
+// Ruta completa de la carpeta de sqlite
+export const SQLITE_PATH: string = path.resolve(__dirname, '../..', '');
