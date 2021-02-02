2fa093e7fed9b3d9cb615f9bec3f965bd341e5e8
Cristian Angel Puma Villalva <cristian.puma.es6@gmail.com>
2021-02-01 22:25:50 -0500

    Se esta agregando un constructor a la entidad User.

--- backend/src/models/user.entity.ts
+++ backend/src/models/user.entity.ts
@@ -21,4 +21,26 @@ export class UserEntity {
     @Column({ nullable: false })
     role: string;
 
+    @Column({ nullable: false })
+    status: boolean;
+
+    constructor(
+        id?: number,
+        nick?: string,
+        name?: string,
+        password?: string,
+        language?: string,
+        role?: string,
+        status?: boolean,
+    ) {
+        this.id = id || null;
+        this.nick = nick || '';
+        this.name = name || '';
+        this.password = password || '';
+        this.language = language || '';
+        this.role = role || '';
+        this.status = status || true;
+    }
+
+
 }
