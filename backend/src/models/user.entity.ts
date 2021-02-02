5bc7fe8fb262d6dd2c1116ca4bad18dbd4e840db
Cristian Angel Puma Villalva <cristian.puma.es6@gmail.com>
2021-02-01 20:54:38 -0500

    Se agregaron los atributos a la entidad User.

--- backend/src/models/user.entity.ts
+++ backend/src/models/user.entity.ts
@@ -1,1 +1,24 @@
-export class UserEntity {}
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity('User')
+export class UserEntity {
+    
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column({ nullable: false })
+    nick: string;
+
+    @Column({ nullable: false })
+    name: string;
+    
+    @Column({ nullable: false })
+    password: string;
+
+    @Column({ nullable: true })
+    language: string;
+
+    @Column({ nullable: false })
+    role: string;
+
+}
