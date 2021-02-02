978668b6fb34951b74a6fcc52f8558c7cf443c13
Cristian Angel Puma Villalva <cristian.puma.es6@gmail.com>
2021-02-01 21:04:24 -0500

    Configuracion Module

--- backend/src/components/users/users.module.ts
+++ backend/src/components/users/users.module.ts
@@ -2,8 +2,20 @@ import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 
+// Dependencias TypeORM
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserEntity } from '../../models/user.entity';
+
+
 @Module({
   providers: [UsersService],
-  controllers: [UsersController]
+  controllers: [UsersController],
+
+  //Importamos el TypeOrm con el modulo a usar, para que funcione en el servicio.
+  imports: [
+    TypeOrmModule.forFeature([UserEntity]),
+  ],
+  // Exportamos el servicio para que lo usemos desde otro lado.
+  exports: [UsersService],
 })
 export class UsersModule {}
