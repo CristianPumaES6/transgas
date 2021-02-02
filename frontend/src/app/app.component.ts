dfd42514d67e86b9f5d1e5286d8a987b398db0d7
Cristian Angel Puma Villalva <cristian.puma.es6@gmail.com>
2021-02-02 18:09:07 -0500

    Se agrego el componenete loading, desde service

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
 
+// Servicio de Loading.
+import { LoadingComponent } from './shared/loading/loading.component';
+import { LoadingService } from './services/loading.service'
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +11,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'frontend';
+
+
+  constructor(
+    private loadingService: LoadingService
+  ) {
+    console.log('constructor()');
+
+
+  };
+
+
+
+  // OnLoadingLoaded => Funcion que inicia el loading.service.
+  public OnLoadingLoaded(loading: LoadingComponent): void {
+    console.log('OnLoadingLoaded(loading: LoadingComponent)');
+
+
+    // Cuando se carga el formulario modal, capturo la referencia y se la envio al servicio
+    this.loadingService.Initialize(loading);
+  }
 }
--- frontend/src/app/global.module.ts
+++ frontend/src/app/global.module.ts
@@ -6,18 +6,37 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
 
+// Components Shared
+import { LoadingComponent } from './shared/loading/loading.component';
+
+// ANGULAR
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+
+// Services
+import { LoadingService } from './services/loading.service';
+
 // Este modulo permite compartir declaraciones con otro modulo.
 @NgModule({
     declarations: [
+        // Components
+        LoadingComponent,
     ],
     imports: [
         CommonModule,
-        ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }),    
+        // PWA
+        ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }),
+        // ANGULAR MATERIAL  
         BrowserAnimationsModule,
+        MatProgressSpinnerModule
     ],
-    exports: [    
+    exports: [
+        // ANGULAR MATERIAL  
         BrowserAnimationsModule,
+        MatProgressSpinnerModule,
 
-    ]
+        // Components
+        LoadingComponent
+    ],
+    providers: [LoadingService]
 })
 export class GlobalModule { }
