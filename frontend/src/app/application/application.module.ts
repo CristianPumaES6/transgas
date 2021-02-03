b260826d43f81c61451f7dfbc83625b7868080ba
Cristian Angel Puma Villalva <cristian.puma.es6@gmail.com>
2021-02-03 17:35:46 -0500

    a-side component, como crear un microcomponente, minicomponente.

--- frontend/src/app/application/application.module.ts
+++ frontend/src/app/application/application.module.ts
@@ -3,14 +3,18 @@ import { CommonModule } from '@angular/common';
 
 import { ApplicationRoutingModule } from './application-routing.module';
 import { DashboardComponent } from '../components/dashboard/dashboard.component';
+
+// Components
 import { ApplicationComponent } from './application.component';
+import { ASideComponent } from '../shared/a-side/a-side.component';
+
 
 
 @NgModule({
-  declarations: [DashboardComponent, ApplicationComponent],
+  declarations: [DashboardComponent, ApplicationComponent, ASideComponent],
   imports: [
     CommonModule,
-    ApplicationRoutingModule
+    ApplicationRoutingModule,
   ]
 })
 export class ApplicationModule { }
---
+++ frontend/src/app/shared/a-side/a-side.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-a-side',
+  templateUrl: './a-side.html',
+  styleUrls: ['./a-side.scss']
+})
+export class  ASideComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
