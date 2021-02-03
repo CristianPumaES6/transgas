0c980a085105776725f92a38c39988a5d8a58acb
Cristian Angel Puma Villalva <cristian.puma.es6@gmail.com>
2021-02-03 11:59:02 -0500

    Auht service componente log-in.

--- frontend/src/app/components/log-in/log-in.component.ts
+++ frontend/src/app/components/log-in/log-in.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
 // ============== COMUNES ==============
-
 // Router
 import { ActivatedRoute, Router } from '@angular/router';
 
@@ -16,6 +15,11 @@ import { NotificationsService } from 'angular2-notifications';
 import { Observable, Subscription, forkJoin, of } from 'rxjs';
 import { map, mergeMap } from 'rxjs/operators';
 // =====================================
+// Models
+import { Login } from '../../models/user'
+
+//Service
+import { AuthService } from '../../services/auth.service';
 
 
 @Component({
@@ -30,17 +34,77 @@ export class LogInComponent implements OnInit {
   public translateCategory: string = 'logIn';
 
 
+  // Armo modelo Login
+  public login: Login = new Login();
+
   constructor(
+    private router: Router,
+    private activatedRoute: ActivatedRoute,
     private loadingService: LoadingService,
-    private languageService: LanguageService
+    private languageService: LanguageService,
+    private authService: AuthService,
+    private notificationsService: NotificationsService,
   ) {
-    console.log("constructor()")
+    console.log("constructor()");
 
   }
 
   ngOnInit(): void {
     console.log('ngOnInit()');
-    alert(this.languageService.GetMessage(this.translateCategory, 'LOG_IN'))
+
+  }
+
+
+  // Cambiar lenguaje
+  public ChangeLanguage(language: string) {
+    console.log("ChangeLanguage(language: string)");
+
+    // Usamos un servicio de language ya creado.
+    this.languageService.SetCurrentLanguage(language);
+    location.reload();
+
+  }
+
+  public Login(): boolean {
+    console.log('Login()');
+    
+    this.loadingService.Open();
+
+    // Mando a hacer el login
+    this.authService.Login(this.login)
+      .subscribe(
+        result => {
+          // Verifico si el login fue exitoso
+          if (result) {
+            // Muestro notificación
+            this.notificationsService.success(this.languageService.GetMessage(this.translateCategory, 'SUCCESS'), this.languageService.GetMessage(this.translateCategory, 'SUCCESS_LOGIN').replace('{{NAME}}', result.name));
+
+            // Todo OK, navego al home
+            this.router.navigate(['./application'], { relativeTo: this.activatedRoute });
+          } else {
+            // Algo fallo, muestro mensaje de error
+            throw 'LOGIN_FAILED';
+          }
+
+          // Deshabilito el spinner de loading
+          this.loadingService.Close();
+        },
+        error => {
+          // Mensaje de error generico
+          let errMsg: string = this.languageService.GetMessage(this.translateCategory, 'ERROR_CONNECTION');
+          // Verifico si hubo error de conexion HTTP y muestro mensaje
+          if (error.status === 0)
+            this.notificationsService.error(this.languageService.GetMessage(this.translateCategory, 'ERROR'), errMsg);
+          else if (error.status > 0)
+            this.notificationsService.error(this.languageService.GetMessage(this.translateCategory, 'ERROR'), errMsg); // Quite estas opciones error.error && error.error.error ||  =>
+          // Deshabilito flag de processing
+          // Deshabilito el spinner de loading
+          this.loadingService.Close();
+        }
+      );
+
+    // Devuelvo false para anular la acción del botón
+    return false;
   }
 
 }
---
+++ frontend/src/app/services/auth.service.ts
@@ -0,0 +1,138 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+import { Observable } from 'rxjs';
+import { map, catchError } from 'rxjs/operators';
+
+import { EnvConfig } from '../config/env.config';
+
+// ============== Modelos ==============
+import { User } from '../models/user';
+import { Login } from '../models/user';
+
+// ============== Componentes ==============
+import { AppComponent } from '../app.component'
+
+// =====================================
+// ============== COMUNES ==============
+// =====================================
+
+// Components Shared
+import { LoadingService } from '../services/loading.service';
+import { LanguageService } from '../services/language.service';
+
+
+import * as Moment from 'moment';
+import * as TimeZone from 'moment-timezone';
+
+// Config
+import { AuthGuardService } from './auth-guard.service';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+
+  // Datos de la session activa
+  private session: string;
+  private loggedUser: User;
+
+  constructor(
+    public http: HttpClient,
+    private httpClient: HttpClient,
+    private languageService: LanguageService,
+    private authGuardService: AuthGuardService,
+  ) {
+
+    // Intento obtener datos almacenados en localStorage
+    try {
+      this.session = localStorage.getItem('Session');
+      this.loggedUser = JSON.parse(localStorage.getItem('LoggedUser'));
+    } catch (err) { }
+
+  }
+
+  // Datos de configuracion del usuario
+  private timeZone: string;
+  public clientOffset: number = 0;
+
+  Login(login: Login): Observable<User> {
+
+    // Armo el request
+    let url: string = EnvConfig.API + '/auth/login';
+
+    let headers: HttpHeaders = new HttpHeaders(
+      {
+        'Content-Type': 'application/json'
+      });
+    let body: string = JSON.stringify(login);
+    let options: any = { headers: headers, responseType: 'json' };
+
+    return this.httpClient.post(url, body, options)
+      .pipe(map(
+        (response: any) => {
+          // Verificamos que la respuesta.
+          if (response.data && response.token) {
+
+            // Guardamos la data y token del usuario.
+            this.loggedUser = response.data;
+            this.session = response.token;
+
+            // Inicializo la zona horaria del cliente
+            this.InitializeTimezone();
+
+            // Seteo el idioma en uso
+            this.languageService.SetCurrentLanguage(this.loggedUser.language);
+
+            // Guardo la sesion y el usario en localStorage
+            localStorage.setItem('Session', this.session);
+            localStorage.setItem('LoggedUser', JSON.stringify(this.loggedUser));
+
+            // Devuelvo true
+            return response.data;
+          } else {
+            // Devuelvo false
+            return false;
+          }
+        }
+      ));
+  }
+
+  GetTimeZone(): string {
+    // Devuelvo la zona horaria del usuario
+    return this.timeZone;
+  }
+
+  InitializeTimezone(): void {
+    // Obtengo el nombre del timezone
+    this.timeZone =  TimeZone.tz.guess();
+    // Obtengo objeto zona de moment, a partir del nombre del timezonetz.zone(this.timeZone)
+    let zone: TimeZone.MomentZone = TimeZone.tz.zone(this.timeZone);
+    // Obtengo diferencia en minutos de utc al timezone, para la fecha actual
+    if (zone) this.clientOffset = -1 * zone.utcOffset(TimeZone().valueOf());
+  }
+
+  GetSession(): string {
+    // Devuelvo la sesion guardada localmente
+    return this.session;
+  }
+
+  GetLoggedUser(): User {
+    // Devuelvo el usuario guardado localmente
+    return this.loggedUser;
+  }
+
+
+  Logout(): boolean {
+
+    localStorage.clear();
+    this.session = null;
+    this.loggedUser = null;
+
+    return false;
+
+  }
+
+}
