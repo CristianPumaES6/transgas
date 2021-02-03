7abbe0a383f70f2ebb2b83be29d29fbeb8af5947
Cristian Angel Puma Villalva <cristian.puma.es6@gmail.com>
2021-02-03 13:04:56 -0500

    ng g component components/dashboard --module application

--- frontend/src/app/application/application.module.ts
+++ frontend/src/app/application/application.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ApplicationRoutingModule } from './application-routing.module';
+import { DashboardComponent } from '../components/dashboard/dashboard.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [DashboardComponent],
   imports: [
     CommonModule,
     ApplicationRoutingModule
