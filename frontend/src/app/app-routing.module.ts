4b7f2c9f7d4ea26a32be43b69ee332507b907829
Cristian Angel Puma Villalva <cristian.puma.es6@gmail.com>
2021-02-03 13:31:15 -0500

    Se agrega la ruta para el nuevo router children.

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -6,6 +6,8 @@ import { LogInComponent } from './components/log-in/log-in.component';
 
 const routes: Routes = [
   { path: '', component: LogInComponent},
+  { path: 'application', redirectTo: '/application', pathMatch: 'full' },
+  { path: 'application/**', redirectTo: '/application', pathMatch: 'full' },
 ];
 
 @NgModule({
