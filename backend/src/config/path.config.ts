3cc4571a17d1a5314bc76ec8b909ee9957542eac
Cristian Angel Puma Villalva <cristian.puma.es6@gmail.com>
2021-02-12 17:03:23 -0500

    Imagen middleware.

--- backend/src/config/path.config.ts
+++ backend/src/config/path.config.ts
@@ -1,5 +1,7 @@
 import * as path from 'path';
 
 
-// Ruta completa de la carpeta de sqlite
+// Ruta completa de la carpeta de sqlite.
 export const SQLITE_PATH: string = path.resolve(__dirname, '../..', '');
+// Ruta de las imagenes subidas.
+export const FOLDER_UPLOADS: string = path.resolve(__dirname, '../..', 'IMAGE_UPLOADS')
