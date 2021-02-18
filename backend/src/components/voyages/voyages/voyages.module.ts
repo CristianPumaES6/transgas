5b3c0d790f67ebc4d711cd42f1e7312304d9ca31
Cristian Angel Puma Villalva <cristian.puma.es6@gmail.com>
2021-02-18 09:53:57 -0500

    nest g service components/voyages/voyages

--- backend/src/components/voyages/voyages/voyages.module.ts
+++ backend/src/components/voyages/voyages/voyages.module.ts
@@ -1,4 +1,7 @@
 import { Module } from '@nestjs/common';
+import { VoyagesService } from './voyages.service';
 
-@Module({})
+@Module({
+  providers: [VoyagesService]
+})
 export class VoyagesModule {}
