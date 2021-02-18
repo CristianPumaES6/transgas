af9f076dea5fdb0a065eb2f6388b3e1e0452f689
Cristian Angel Puma Villalva <cristian.puma.es6@gmail.com>
2021-02-18 09:43:00 -0500

    Se estan creando las entidades. Voyage, Port, Daily-report

---
+++ backend/src/models/daily-report.entity.ts
@@ -0,0 +1,110 @@
+import { Entity, Column, PrimaryGeneratedColumn, ManyToOne } from 'typeorm';
+import { Port } from './port.entity';
+
+@Entity()
+export class DailyReport {
+
+    // Id Detalle
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    // UserId que registra el dato
+    @Column()
+    userId: number;
+
+    // Viaje ID
+    @Column()
+    portId: number;
+    @ManyToOne(type => Port, port => port.id )
+    port: Port;
+
+    
+
+    // actividad Registrada
+    @Column({ default: "Otros" })
+    activityPerformed: string;
+
+    // Fecha de registro
+    @Column()
+    date: Date;
+
+    // Hora
+    @Column()
+    hour: string;
+
+    // Recarga de IFO
+    @Column({ default: 0 })
+    bunkeringIfo: number;
+
+    // Recarga de MGO
+    @Column({ default: 0 })
+    bunkeringMgo: number;
+
+    // robIfo
+    // @Column()
+    // robIfo: string;
+    @Column({ default: 0 })
+    mplaIfo: number;
+
+    @Column({ default: 0 })
+    auxIfo: number;
+
+    @Column({ default: 0 })
+    boilerIfo: number;
+
+    @Column({ default: 0 })
+    otherIfo: number;
+    // -- Fin Ifo --
+
+    // robMgo
+    // @Column()
+    // robMgo: string;
+    @Column({ default: 0 })
+    mplaMgo: number;
+
+    @Column({ default: 0 })
+    auxMgo: number;
+
+    @Column({ default: 0 })
+    boilerMgo: number;
+
+    @Column({ default: 0 })
+    ppMgo: number;
+
+    @Column({ default: 0 })
+    giMgo: number;
+
+    @Column({ default: 0 })
+    otherMgo: number;
+    // Fin MGO
+
+    // Tempo navegando
+    @Column({ default: 0 })
+    steamingTime: number;
+    // Distancia
+    @Column({ default: 0 })
+    distance: number;
+    // beaufour
+    @Column()
+    beaufour: string;
+    // Observaciones
+    @Column()
+    observation: string;
+
+
+
+    // Auditoria
+    @Column()
+    userIdCreated: number;
+    @Column()
+    dateCreated: string;
+
+    @Column({ nullable: true })
+    userIdUpdated: number;
+    @Column({ nullable: true })
+    dateUpdated: string;
+
+    @Column({ nullable: false })
+    status: boolean;
+
+}
---
+++ backend/src/models/port.entity.ts
@@ -0,0 +1,54 @@
+import { Entity, Column, PrimaryGeneratedColumn, ManyToOne, OneToMany } from 'typeorm';
+import { DailyReport } from './daily-report.entity';
+import { Voyage } from './voyage.entity';
+
+@Entity()
+export class Port {
+
+    // Id unique.
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    // userId : servira para hacer auditoria.
+    @Column()
+    userId: number;
+
+    // Numero de viaje.
+    @Column()
+    voyageId: number;
+    @ManyToOne(type => Voyage, voyage => voyage.id)
+    voyage: Voyage;
+    @OneToMany(type => DailyReport, dailyReport => dailyReport.port, {
+                                                                        eager: true,
+                                                                        cascade: true
+                                                                    })
+    dailyReports: DailyReport[];
+
+    // Numero de viaje.
+    @Column()
+    portNumber: number;
+
+    // Lugar de partida.
+    @Column()
+    departurePort: string;
+
+    // Lugar de llegada.
+    @Column()
+    arrivalPort: string;
+
+
+    // Auditoria
+    @Column()
+    userIdCreated: number;
+    @Column()
+    dateCreated: string;
+
+    @Column({ nullable: true })
+    userIdUpdated: number;
+    @Column({ nullable: true })
+    dateUpdated: string;
+
+    @Column({ nullable: false })
+    status: boolean;
+
+}
