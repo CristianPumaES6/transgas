224c6532fdb1dfc8ad58bb9d507b3b17fd71c829
Cristian Angel Puma Villalva <cristian.puma.es6@gmail.com>
2021-02-18 09:54:44 -0500

    nest g controller components/voyages/voyages

---
+++ backend/src/components/voyages/voyages/voyages.controller.ts
@@ -0,0 +1,4 @@
+import { Controller } from '@nestjs/common';
+
+@Controller('voyages')
+export class VoyagesController {}
--- backend/src/components/voyages/voyages/voyages.module.ts
+++ backend/src/components/voyages/voyages/voyages.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { VoyagesService } from './voyages.service';
+import { VoyagesController } from './voyages.controller';
 
 @Module({
-  providers: [VoyagesService]
+  providers: [VoyagesService],
+  controllers: [VoyagesController]
 })
 export class VoyagesModule {}
