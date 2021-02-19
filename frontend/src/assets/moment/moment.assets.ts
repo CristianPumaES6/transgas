5e5d3f405a62682765c446ec89c77c6b18de4ff9
Cristian Angel Puma Villalva <cristian.puma.es6@gmail.com>
2021-02-19 10:17:45 -0500

    Se esta agregando moment y time-zone al front

---
+++ frontend/src/assets/moment/moment.assets.ts
@@ -0,0 +1,18 @@
+import * as moment from 'moment';
+import * as momentTimezone from 'moment-timezone';
+
+moment.locale();
+momentTimezone().tz("America/Los_Angeles").format();
+
+// AL ser una class lo tengo que poner en el constructor para que pueda ser utilizado.
+export function getDate(): any {
+
+    // lo desencripto
+    return moment().format();
+}
+
+export function getYear(): string {
+
+    // retornamos el año.
+    return moment().format('YYYY');
+}
