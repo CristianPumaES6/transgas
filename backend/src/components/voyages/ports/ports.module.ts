bbd4f63eac1ad1dceff021bdf2dc8ec0dd9e3e9f
Cristian Angel Puma Villalva <cristian.puma.es6@gmail.com>
2021-02-19 19:52:47 -0500

    nest g controller components/voyages/ports

--- backend/src/components/voyages/ports/ports.module.ts
+++ backend/src/components/voyages/ports/ports.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { PortsService } from './ports.service';
+import { PortsController } from './ports.controller';
 
 @Module({
-  providers: [PortsService]
+  providers: [PortsService],
+  controllers: [PortsController]
 })
 export class PortsModule {
     
