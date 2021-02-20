93bbec74bc4b12ec0dd9ec5e23cc59f14f27e466
Cristian Angel Puma Villalva <cristian.puma.es6@gmail.com>
2021-02-19 21:46:32 -0500

    port controller

--- backend/src/components/voyages/ports/ports.controller.ts
+++ backend/src/components/voyages/ports/ports.controller.ts
@@ -1,4 +1,363 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Headers, HttpException, HttpStatus, Param, Post, Put, Query } from '@nestjs/common';
+import { JwtDecode } from 'src/assets/jwtDecode.assets';
+import { getDate } from 'src/assets/moment.assets';
+import { DummyPromise } from 'src/assets/promises.assets';
+import { Port } from 'src/models/port.entity';
+import { UserEntity } from 'src/models/user.entity';
+import { PortsService } from './ports.service';
 
 @Controller('ports')
-export class PortsController {}
+export class PortsController {
+
+    constructor(
+        private readonly _portsService: PortsService,
+    ) { }
+
+    @Get('detail')
+    async GetsDetail(@Headers() headers, @Query() port: Port): Promise<any> {
+
+        // Le asigno el valor al token desde la cabecera.
+        // Lo decodifico con otra libreria por problemas jwt-module.
+        let headerToken: UserEntity = JwtDecode(headers.authorization);
+
+        // Inicio una promesa Dummy.
+        return DummyPromise().then(
+            (resultDummy: Boolean) => {
+                // Validamos que los datos sean los necesarios.
+                if (port && port.userId) {
+
+                    port.userId = Number(port.userId);
+                    return true;
+
+                } else throw new Error('MISSING_FIELS');
+
+            }
+        ).then(
+            (resultValidate: Boolean) => {
+                // Validamos que el userId sea el mismo que el del token
+                if (headerToken.role === 'ADMIN') {
+                    port.userId = null;
+                } else if (port.userId !== headerToken.id) throw new Error('ERROR_USERID_FAIL');
+
+                // Ejecutamos el servicio de obtener sailingAnalities.
+                return this._portsService.GetsDetail(port);
+            }
+        ).then(
+            (results: Port[]) => {
+
+                // Retornamos una Respuesta exitosa.
+                return {
+                    status: HttpStatus.OK,
+                    message: 'OK',
+                    data: results
+                };
+            }
+        ).catch(
+            err => {
+                // Obtengo mensajes de error
+                const clientMsg: string = (typeof err === 'string' ? err : 'CANNOT_PROCESS_REQUEST');
+                const errorMsg: string = (typeof err === 'string' ? err : err.message || err.description || 'ERROR_EXEC_REQUEST');
+
+                // Caso contrario retornamos un error
+                throw new HttpException({
+                    status: HttpStatus.ACCEPTED,
+                    error: clientMsg,
+                    message: errorMsg,
+                }, HttpStatus.ACCEPTED);
+            }
+        );
+    }
+
+    @Get(':id')
+    async Get(@Headers() headers, @Param('id') id): Promise<any> {
+
+        // Le asigno el valor al token desde la cabecera.
+        // Lo decodifico con otra libreria por problemas jwt-module.
+        let headerToken: UserEntity = JwtDecode(headers.authorization);
+
+        // Inicio una promesa Dummy.
+        return DummyPromise().then(
+            (resultDummy: Boolean) => {
+
+                // Validamos que los datos recibidos sean los correctos.
+                if (Number(id)) {
+                    let portId = Number(id);
+                    return this._portsService.Get(portId);
+                } else {
+                    // caso contrario retornamos un error
+                    throw 'MISSING_FIELS';
+                }
+
+            }
+        ).then(
+            (resultGet: Port) => {
+
+                // Validamos que el userId sea el mismo que el del token
+                if (headerToken.role == 'ADMIN' || headerToken.role == 'SUPPORT') {
+
+                } else if (Number(resultGet.userId) !== Number(headerToken.id)) {
+                    throw new Error('ERROR_USERID_FAIL');
+                }
+
+                // retornamos una Respuesta exitosa.
+                return {
+                    status: HttpStatus.ACCEPTED,
+                    message: 'OK',
+                    data: resultGet
+                };
+            }
+        ).catch(
+            err => {
+                // Obtengo mensajes de error
+                const clientMsg: string = (typeof err === 'string' ? err : 'CANNOT_PROCESS_REQUEST');
+                const errorMsg: string = (typeof err === 'string' ? err : err.message || err.description || 'ERROR_EXEC_REQUEST');
+
+                // caso contrario retornamos un error
+                throw new HttpException({
+                    status: HttpStatus.NOT_ACCEPTABLE,
+                    error: clientMsg,
+                    message: errorMsg,
+                }, HttpStatus.NOT_ACCEPTABLE);
+            }
+        );
+    }
+
+    @Get()
+    async Gets(@Headers() headers, @Query() port: Port): Promise<any> {
+
+        // Le asigno el valor al token desde la cabecera.
+        // Lo decodifico con otra libreria por problemas jwt-module.
+        let headerToken: UserEntity = JwtDecode(headers.authorization);
+
+        // Inicio una promesa Dummy.
+        return DummyPromise().then(
+            (resultDummy: Boolean) => {
+                // Validamos que los datos sean los necesarios.
+                if (port && port.userId) {
+
+                    port.userId = Number(port.userId);
+                    return true;
+
+                } else throw new Error('MISSING_FIELS');
+
+            }
+        ).then(
+            (resultValidate: Boolean) => {
+                // Validamos que el userId sea el mismo que el del token
+                if (headerToken.role == 'ADMIN') {
+                    port.userId = null;
+                } else if (port.userId !== headerToken.id) throw new Error('ERROR_USERID_FAIL');
+
+                // Ejecutamos el servicio de obtener sailingAnalities.
+                return this._portsService.Gets(port);
+            }
+        ).then(
+            (results: Port[]) => {
+
+                // Retornamos una Respuesta exitosa.
+                return {
+                    status: HttpStatus.OK,
+                    message: 'OK',
+                    data: results
+                };
+            }
+        ).catch(
+            err => {
+                // Obtengo mensajes de error
+                const clientMsg: string = (typeof err === 'string' ? err : 'CANNOT_PROCESS_REQUEST');
+                const errorMsg: string = (typeof err === 'string' ? err : err.message || err.description || 'ERROR_EXEC_REQUEST');
+
+                // Caso contrario retornamos un error
+                throw new HttpException({
+                    status: HttpStatus.ACCEPTED,
+                    error: clientMsg,
+                    message: errorMsg,
+                }, HttpStatus.ACCEPTED);
+            }
+        );
+    }
+
+    @Post('create')
+    async CreatePort(@Headers() headers, @Body() port: Port): Promise<any> {
+
+        // Le asigno el valor al token desde la cabecera.
+        // Lo decodifico con otra libreria por problemas jwt-module.
+        let headerToken: UserEntity = JwtDecode(headers.authorization);
+
+        return DummyPromise().then(
+            (resultDummy: Boolean) => {
+                // Validamos que esten llegando los datos necesarios.
+                if (port && port.userId && Number(port.portNumber) && port.departurePort && headerToken && headerToken.id) {
+
+                    if (headerToken.role !== 'ADMIN' && headerToken.role !== 'SUPPORT') {
+                        if (Number(headerToken.id) !== Number(port.userId)) throw new Error('ERROR_USERID_FAIL');
+                    }
+
+                    // Auditoria.
+                    port.userIdCreated = headerToken.id;
+                    port.dateCreated = getDate();
+                    delete port.userIdUpdated;
+                    delete port.dateUpdated;
+                    port.status = Boolean(port.status);
+
+                    // Ejecutamos la funcion que registra en bd.
+                    return this._portsService.Create(port);
+                }
+                else throw 'MISSING_FIELS';
+            }
+        ).then(
+            (resultCreate: Port) => {
+
+                // retornamos una Respuesta exitosa.
+                return {
+                    status: HttpStatus.OK,
+                    message: 'OK',
+                    data: resultCreate
+                };
+            }
+        ).catch(
+            err => {
+                // Obtengo mensajes de error
+                const clientMsg: string = (typeof err === 'string' ? err : 'CANNOT_PROCESS_REQUEST');
+                const errorMsg: string = (typeof err === 'string' ? err : err.message || err.description || 'ERROR_EXEC_REQUEST');
+
+                // caso contrario retornamos un error
+                throw new HttpException({
+                    status: HttpStatus.ACCEPTED,
+                    error: clientMsg,
+                    message: errorMsg,
+                }, HttpStatus.ACCEPTED);
+            }
+        );;
+    }
+
+    @Put(':id/update')
+    async Update(@Headers() headers, @Param('id') id, @Body() port: Port): Promise<any> {
+
+        // Le asigno el valor al token desde la cabecera.
+        // Lo decodifico con otra libreria por problemas jwt-module.
+        let headerToken: UserEntity = JwtDecode(headers.authorization);
+
+        // Inicio una promesa Dummy.
+        return DummyPromise().then(
+            (resultDummy: Boolean) => {
+
+                // Validamos los datos del objeto a registar.
+                if (port && port.userId && port.portNumber && port.departurePort && headerToken && headerToken.id) {
+
+                    // Validamos los datos obtenidos.
+                    if (headerToken.role === 'BUQUE') {
+                        if (Number(headerToken.id) !== Number(port.userId)) throw new Error('ERROR_USERID_FAIL');
+                    }
+                    
+                    // Auditoria.
+                    delete port.userIdCreated;
+                    delete port.dateCreated;
+                    port.userIdUpdated = headerToken.id;
+                    port.dateUpdated = getDate();
+                    port.status = Boolean(port.status);
+                    
+
+                    // Ejecutamos la funcion que actualiza el obj en la bd.
+                    return this._portsService.Update(port);
+
+                } else {
+
+                    // Enviar los datos necesarios.
+                    throw 'MISSING_FIELS';
+
+                }
+            }
+        ).then(
+            (resultUpdate: Port) => {
+
+                // Validamos el resultado
+                if (!resultUpdate) throw new Error('TYPEORM_UPDATE_VOYAGE');
+
+                // Retornamos una! Respuesta exitosa.
+                return {
+                    status: HttpStatus.OK,
+                    message: 'OK',
+                    data: resultUpdate
+                };
+            }
+        ).catch(
+            err => {
+                // Obtengo mensajes de error
+                const clientMsg: string = (typeof err === 'string' ? err : 'CANNOT_PROCESS_REQUEST');
+                const errorMsg: string = (typeof err === 'string' ? err : err.message || err.description || 'ERROR_EXEC_REQUEST');
+
+                // Caso contrario retornamos un error
+                throw new HttpException({
+                    status: HttpStatus.NOT_ACCEPTABLE,
+                    error: clientMsg,
+                    message: errorMsg,
+                }, HttpStatus.ACCEPTED);
+            }
+        );
+    }
+
+    @Delete(':id/delete')
+    async Delete(@Headers() headers, @Param('id') id): Promise<any> {
+
+        // Le asigno el valor al token desde la cabecera.
+        // Lo decodifico con otra libreria por problemas jwt-module.
+        let headerToken: UserEntity = JwtDecode(headers.authorization);
+
+        // Inicio una promesa Dummy.
+        return DummyPromise().then(
+            (resultDummy: Boolean) => {
+
+                // Validamos que los datos sean los necesarios.
+                if (Number(id)) {
+                    // Decodeamos.
+                    return this._portsService.Get(id);
+
+                } else {
+                    // Enviar los datos necesarios.
+                    throw new Error('MISSING_FIELS');
+
+                }
+
+            }
+        ).then(
+            (result: Port) => {
+                delete result.dailyReports;
+
+                result.status = false;
+                delete result.userIdCreated;
+                delete result.dateCreated;
+                result.userIdUpdated = headerToken.id;
+                result.dateUpdated = getDate();
+
+                return this._portsService.Delete(result);
+            }
+        ).then(
+            (resultDelete: Port) => {
+
+                // Retornamos una Respuesta exitosa.
+                return {
+                    status: HttpStatus.ACCEPTED,
+                    message: 'OK',
+                    data: resultDelete
+                };
+            }
+        ).catch(
+            err => {
+                // Obtengo mensajes de error
+                const clientMsg: string = (typeof err === 'string' ? err : 'CANNOT_PROCESS_REQUEST');
+                const errorMsg: string = (typeof err === 'string' ? err : err.message || err.description || 'ERROR_EXEC_REQUEST');
+
+                // caso contrario retornamos un error
+                throw new HttpException({
+                    status: HttpStatus.NOT_ACCEPTABLE,
+                    error: clientMsg,
+                    message: errorMsg,
+                }, HttpStatus.NOT_ACCEPTABLE);
+            }
+        );
+
+    }
+
+
+}
