ec160208cafbe727d0cfd895ab1e80b78ab3131c
Cristian Angel Puma Villalva <cristian.puma.es6@gmail.com>
2021-02-25 11:17:09 -0500

    nest g service components/voyages/daily-reports

--- backend/src/components/voyages/daily-reports/daily-reports.module.ts
+++ backend/src/components/voyages/daily-reports/daily-reports.module.ts
@@ -1,4 +1,7 @@
 import { Module } from '@nestjs/common';
+import { DailyReportsService } from './daily-reports.service';
 
-@Module({})
+@Module({
+  providers: [DailyReportsService]
+})
 export class DailyReportsModule {}
