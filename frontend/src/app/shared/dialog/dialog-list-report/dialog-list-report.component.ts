94bb8bcfddd6e671488ae399890d2b1593d10bcc
Cristian Angel Puma Villalva <cristian.puma.es6@gmail.com>
2021-03-25 19:10:05 -0700

    Primer diseño de dialog como prueba de funcionalidad.

--- frontend/src/app/shared/dialog/dialog-list-report/dialog-list-report.component.ts
+++ frontend/src/app/shared/dialog/dialog-list-report/dialog-list-report.component.ts
@@ -1,4 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+
+// Interface de la clase del componente
+export interface IDialogListReport {
+  reporte: string,
+}
+
 
 @Component({
   selector: 'app-dialog-list-report',
@@ -7,9 +14,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DialogListReportComponent implements OnInit {
 
-  constructor() { }
+  // Constructores para setear valores al componente.
+  constructor(
+    public dialogRef: MatDialogRef<DialogListReportComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: IDialogListReport
+  ) { }
 
-  ngOnInit(): void {
+  // Inicializamos el componente
+  ngOnInit() {
+    console.log('ngOnInit() ');
+  }
+
+  // Evento no click.
+  onNoClick(): void {
+    this.dialogRef.close();
   }
 
 }
