d0165f05e698801263a72677b3c1f52c54573897
CristianPumaES6 <cristian.puma.es6@gmail.com>
2021-04-20 14:36:15 -0400

    Inteface loggedUser

---
+++ backend/src/models/loggedUser.ts
@@ -0,0 +1,14 @@
+
+export class LoggedUser {
+    token:string;
+    userName:string;
+
+    constructor(
+        token?:string,
+        userName?:string,
+    ){
+        this.token = token || '';
+        this.userName = userName || '';
+    }
+
+}
