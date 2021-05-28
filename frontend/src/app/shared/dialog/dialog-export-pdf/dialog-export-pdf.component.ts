aaf7c6c7883269012bb080f2fe053c869620ffbc
CristianPumaES6 <cristian.puma.es6@gmail.com>
2021-05-27 21:47:33 -0400

    dialog.

--- frontend/src/app/shared/dialog/dialog-export-pdf/dialog-export-pdf.component.ts
+++ frontend/src/app/shared/dialog/dialog-export-pdf/dialog-export-pdf.component.ts
@@ -1,4 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { Port } from '../../../models/port';
+import { User } from '../../../models/user';
+import { Voyage } from '../../../models/voyage';
+import { LanguageService } from '../../../services/language.service';
+import { DialogListReportComponent } from '../dialog-list-report/dialog-list-report.component';
+
+// Interface de los input del componente.
+export interface IDialogExportPdf {
+  voyage: Voyage,
+  port: Port,
+  selectUser: User,
+}
 
 @Component({
   selector: 'app-dialog-export-pdf',
@@ -7,9 +20,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DialogExportPdfComponent implements OnInit {
 
-  constructor() { }
+  // Constructores para setear valores al componente.
+  constructor(
+    // Dialog referencia es el mismo.
+    public dialogRef: MatDialogRef<DialogExportPdfComponent>,
+    // Data que se importara.
+    @Inject(MAT_DIALOG_DATA) public data: IDialogExportPdf,
+    // servicio de lenguaje.
+    private languageService: LanguageService,
+  ) { }
+
+  // Traducciones
+  public userLanguage: string = this.languageService.GetCurrentLanguage();
+  public translateCategory: string = 'dialog';
+
+  // Usuario seleccionado
+  public selectUser:User=new User();
 
   ngOnInit(): void {
+    this.selectUser = this.data.selectUser;
   }
 
 }
