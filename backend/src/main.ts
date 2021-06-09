203788539f54977e1ef84501a2583fde099a75c4
CristianPumaES6 <cristian.puma.es6@gmail.com>
2021-06-09 00:52:56 -0400

    Se agrego una carpeta estatica en el backend para las imagenes.

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 
 // Express
 import * as express from 'express';
-import { FOLDER_UPLOADS } from './config/path.config';
+import { FOLDER_UPLOADS,FOLDER_STATIC } from './config/path.config';
 import { join } from 'path';
 
 async function bootstrap() {
@@ -22,6 +22,8 @@ async function bootstrap() {
   app.enableCors(options);
 
   app.use(express.static(join(FOLDER_UPLOADS)));
+
+  app.use(express.static(join(FOLDER_STATIC)));
 
   await app.listen(3000);
 }
