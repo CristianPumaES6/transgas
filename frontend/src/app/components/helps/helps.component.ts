478afd69f0442473622ace0a03e3d2cf41c8bfa7
CristianPumaES6 <cristian.puma.es6@gmail.com>
2021-06-09 00:58:30 -0400

    Arreglo de las imagens

--- frontend/src/app/components/helps/helps.component.ts
+++ frontend/src/app/components/helps/helps.component.ts
@@ -9,6 +9,19 @@ import PerfectScrollbar from 'perfect-scrollbar';
 })
 export class HelpsComponent implements OnInit {
 
+  // Variable que contiene todas las url de imagenes.
+  public arrImgHome:string[] = [
+    "http://localhost:3000/img/1Home.png",
+    "http://localhost:3000/img/2Voyage.png",
+    "http://localhost:3000/img/4Report.png",
+    "http://localhost:3000/img/5Dashboard.png",
+    "http://localhost:3000/img/6Dashboard2.png",
+    "http://localhost:3000/img/7Dashboard3.png",
+    "http://localhost:3000/img/8Users.png",
+    "http://localhost:3000/img/9Users2.png",
+    "http://localhost:3000/img/1Home.png"
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
