e1c6da5d48a2168da74e6e2a980036e70742d88b
CristianPumaES6 <cristian.puma.es6@gmail.com>
2021-06-10 04:32:39 -0400

    ok

--- bash/UploadFrontEnd.sh
+++ bash/UploadFrontEnd.sh
@@ -4,9 +4,17 @@
 # Nos ubicamos en el directorio frontend para eliminar el directorio dist
 cd ..
 cd frontend
-
+# REmovemos el directorio.
 rm -rf dist
-# generamos el build
-ng build --prod
 
-firebase deploy
+# Nos ubicamos en el config del frontend
+cd src
+cd app
+cd config
+# Lo editamos.
+sed -i "s%http://localhost:3000%https://transgas.codev.site%g" "env.config.ts"
+
+
+sed -i "s%https://transgas.codev.site%http://localhost:3000%g" "env.config.ts"
+
+echo "OK Deploy."
