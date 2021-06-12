e6f9b0f8881da0d9631b79ab1d3eeb3ea95ad516
CristianPumaES6 <cristian.puma.es6@gmail.com>
2021-06-12 11:10:55 -0400

    Se agrego una nueva variable a la configuracion del front y back.

--- backend/src/config/server.config.ts
+++ backend/src/config/server.config.ts
@@ -1,6 +1,7 @@
 /* export const URL_Server = {
     front: 'https://transgas.web.app',
     back: 'https://transgas.labcode.site',
+    version: 'v1.9a'
 }; */
 
 
@@ -8,6 +9,7 @@
 /* export const URL_Server = {
     front: 'https://transgas.web.app',
     back: 'https://transgas.codev.site',
+    version: 'v1.9a'
 };
  */
 
@@ -16,4 +18,5 @@
 export const URL_Server = {
     front: 'http://localhost:3001',
     back: 'http://localhost:3000',
+    version: 'v1.9a'
 };
