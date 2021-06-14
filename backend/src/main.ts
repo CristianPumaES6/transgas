c79d78cbe5526a81b383c9b89259ce74d4f39d6c
CristianPumaES6 <cristian.puma.es6@gmail.com>
2021-06-14 01:13:21 -0400

    Se agrego el main.ts al backend

---
+++ backend/src/main.ts
@@ -0,0 +1,30 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+// Express
+import * as express from 'express';
+import { FOLDER_UPLOADS,FOLDER_STATIC } from './config/path.config';
+import { join } from 'path';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  const options = {
+    'origin': '*',
+    'methods': 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    'preflightContinue': false,
+    'optionsSuccessStatus': 204,
+    'credentials': true,
+  };
+  
+  // Habilitamos el CORS
+  // app.use(cors(options))
+  app.enableCors(options);
+
+  app.use(express.static(join(FOLDER_UPLOADS)));
+
+  app.use(express.static(join(FOLDER_STATIC)));
+
+  await app.listen(3000);
+}
+bootstrap();
