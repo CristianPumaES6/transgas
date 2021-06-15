7e14440f9a05fc64bfad2f81682477d4b5af8daa
CristianPumaES6 <cristian.puma.es6@gmail.com>
2021-06-15 11:14:35 -0400

    Update server version 1.9e

--- backend/dist/config/server.config.js
+++ backend/dist/config/server.config.js
@@ -4,6 +4,6 @@ exports.URL_Server = void 0;
 exports.URL_Server = {
     front: 'https://transgas.web.app',
     back: 'https://transgas.codev.site',
-    version: 'v1.9c'
+    version: 'v1.9e'
 };
 //# sourceMappingURL=server.config.js.map
