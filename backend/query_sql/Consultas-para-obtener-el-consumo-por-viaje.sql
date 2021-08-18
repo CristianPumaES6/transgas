a8cafe20591681d4e19da542e1968370ad262b8e
CristianPumaES6 <cristian.puma.es6@gmail.com>
2021-08-17 23:25:37 -0400

    sql

---
+++ backend/query_sql/Consultas-para-obtener-el-consumo-por-viaje.sql
@@ -0,0 +1,44 @@
+-- Obtenemos la fecha de inicio, fecha fin del viaje, ademas el consumo que se hizo en el viaje.
+SELECT
+	port.voyageId,
+	voyage.voyageNumber,
+	MIN(daily_report.date),
+	MAX(daily_report.date),		
+	SUM( daily_report.mplaIfo + daily_report.auxIfo + daily_report.boilerIfo + daily_report.otherIfo ) AS 'total_ifo',
+	SUM( daily_report.mplaMgo + daily_report.auxMgo + daily_report.boilerMgo + daily_report.ppMgo + daily_report.giMgo + daily_report.otherMgo ) AS 'total_mgo'
+
+FROM daily_report
+	INNER JOIN port ON daily_report.portId = port.id
+	INNER JOIN voyage ON port.voyageId = voyage.id
+
+WHERE daily_report.status = 1 
+	AND port.status = 1
+	AND voyage.status = 1
+
+GROUP BY port.voyageId, voyage.voyageNumber
+
+
+-- Obtenemos los bunkering registrados en el viaje.
+
+SELECT
+	voyage.id,
+	voyage.voyageNumber,
+	port.id,
+	port.voyageId,
+	port.portNumber,
+	port.departurePort,
+	daily_report.id,
+	daily_report.bunkeringIfo,
+	daily_report.bunkeringMgo,
+	daily_report.observation
+	
+FROM daily_report
+	INNER JOIN port ON daily_report.portId = port.id
+	INNER JOIN voyage ON port.voyageId = voyage.id
+
+WHERE daily_report.status = 1 
+	AND port.status = 1
+	AND voyage.status = 1
+	AND (daily_report.bunkeringIfo > 0 OR daily_report.bunkeringMgo > 0)
+
+
