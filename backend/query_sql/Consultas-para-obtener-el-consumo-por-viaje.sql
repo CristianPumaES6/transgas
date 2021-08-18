baece9b7989f6e1687c2c878c6db34a349c1c9e5
CristianPumaES6 <cristian.puma.es6@gmail.com>
2021-08-18 17:26:46 -0400

    QUery del rob y bunkering.

--- backend/query_sql/Consultas-para-obtener-el-consumo-por-viaje.sql
+++ backend/query_sql/Consultas-para-obtener-el-consumo-por-viaje.sql
@@ -1,6 +1,6 @@
 -- Obtenemos la fecha de inicio, fecha fin del viaje, ademas el consumo que se hizo en el viaje.
 SELECT
-	port.voyageId,
+	voyage.id,
 	voyage.voyageNumber,
 	MIN(daily_report.date),
 	MAX(daily_report.date),		
@@ -15,7 +15,7 @@ WHERE daily_report.status = 1
 	AND port.status = 1
 	AND voyage.status = 1
 
-GROUP BY port.voyageId, voyage.voyageNumber
+GROUP BY voyage.id, voyage.voyageNumber
 
 
 -- Obtenemos los bunkering registrados en el viaje.
@@ -40,5 +40,35 @@ WHERE daily_report.status = 1
 	AND port.status = 1
 	AND voyage.status = 1
 	AND (daily_report.bunkeringIfo > 0 OR daily_report.bunkeringMgo > 0)
+
+
+
+
+-- ṕr íertp
+SELECT
+	voyage.id AS 'voyage.id',
+	voyage.voyageNumber AS 'voyage.voyageNumber',
+	port.id AS 'port.id',
+	port.voyageId AS 'port.voyageId',
+	port.portNumber AS 'port.portNumber',
+	MIN(daily_report.date),
+	MAX(daily_report.date),		
+	SUM( daily_report.mplaIfo + daily_report.auxIfo + daily_report.boilerIfo + daily_report.otherIfo ) AS 'total_ifo',
+	SUM( daily_report.mplaMgo + daily_report.auxMgo + daily_report.boilerMgo + daily_report.ppMgo + daily_report.giMgo + daily_report.otherMgo ) AS 'total_mgo'
+
+FROM daily_report
+	INNER JOIN port ON daily_report.portId = port.id
+	INNER JOIN voyage ON port.voyageId = voyage.id
+
+WHERE daily_report.status = 1 
+	AND port.status = 1
+	AND voyage.status = 1
+
+GROUP BY 	
+	voyage.id,
+	voyage.voyageNumber,
+	port.id,
+	port.voyageId,
+	port.portNumber
 
 
