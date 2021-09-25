14f3cbcfda6359044c5bbfd17f89ea4ef6d87200
CristianPumaES6 <cristian.puma.es6@gmail.com>
2021-09-25 18:47:58 -0500

    Excel cuadro de velocidad

--- frontend/src/app/services/excel.service.ts
+++ frontend/src/app/services/excel.service.ts
@@ -532,17 +532,9 @@ export class ExcelService {
 
 
 
-    posicion = [position, position + 1];
-    posicionColumn = [44, 45];
-    this.addStyleByColums(worksheet, posicion, posicionColumn, 'SPEED', 10, colorYellowTransgas, blueHard3, '')
+    positionColum = 44;
+    let tamanioSpeed = this.StyleDashSpeed(worksheet, positionRow, positionColum, selectUser);
 
-    posicion = [position, position];
-    posicionColumn = [46, 47];
-    this.addStyleByColums(worksheet, posicion, posicionColumn, 'CHARTER', 8, white, blueHard3, '')
-
-    posicion = [position, position];
-    posicionColumn = [48, 49];
-    this.addStyleByColums(worksheet, posicion, posicionColumn, 'PERFORMEND', 8, white, blueHard2, '')
 
 
     posicion = [position, position];
@@ -575,22 +567,6 @@ export class ExcelService {
 
     // ================= Linea 4
 
-
-    // FULL Y ECO Charter SPEED
-    posicion = [position, position];
-    posicionColumn = [46, 46];
-    this.addStyleByColums(worksheet, posicion, posicionColumn, 'FULL', 8, white, blueHard3, '')
-    posicion = [position, position];
-    posicionColumn = [47, 47];
-    this.addStyleByColums(worksheet, posicion, posicionColumn, 'ECO', 8, white, blueHard3, '')
-
-    // FULL Y ECO Performan SPEED
-    posicion = [position, position];
-    posicionColumn = [48, 48];
-    this.addStyleByColums(worksheet, posicion, posicionColumn, 'FULL', 8, white, blueHard2, '')
-    posicion = [position, position];
-    posicionColumn = [49, 49];
-    this.addStyleByColums(worksheet, posicion, posicionColumn, 'ECO', 8, white, blueHard2, '')
 
     // Actividades. dailyconsumption
     posicion = [position, position];
@@ -636,25 +612,6 @@ export class ExcelService {
 
     // ================= Linea 5
 
-
-    // FULL Y ECO Charter SPEED IFO
-    posicion = [position, position];
-    posicionColumn = [44, 45];
-    this.addStyleByColums(worksheet, posicion, posicionColumn, 'Ballast', 8, black, white, '');
-    posicion = [position, position];
-    posicionColumn = [46, 46];
-    this.addStyleByColums(worksheet, posicion, posicionColumn, selectUser.sailingBallastConsumptionIFO, 8, black, white, '');
-    posicion = [position, position];
-    posicionColumn = [47, 47];
-    this.addStyleByColums(worksheet, posicion, posicionColumn, selectUser.sailingEconomicConsumptionIFO, 8, black, white, '');
-
-    // FULL Y ECO Performan SPEED
-    posicion = [position, position];
-    posicionColumn = [48, 48];
-    this.addStyleByColums(worksheet, posicion, posicionColumn, 'codifo del performent', 8, black, white, '');
-    posicion = [position, position];
-    posicionColumn = [49, 49];
-    this.addStyleByColums(worksheet, posicion, posicionColumn, 'codifo del performent', 8, black, white, '')
 
     // Actividades. dailyconsumption
     posicion = [position, position];
@@ -2522,6 +2479,147 @@ export class ExcelService {
     this.addBorder(worksheet, posit - 3, colum + 5, '', blueHard3, 'top')
     this.addBorder(worksheet, posit - 3, colum + 8, '', blueHard3, 'top')
 
+    return posit;
+  }
+
+
+  private StyleDashSpeed(worksheet, posit, colum, selectUser: User): number {
+    let date_start = '22/22/22'
+    let hour_start = '20:20'
+    let ifo_start = 200;
+    let mgo_start = 300;
+    let date_end = '22/22/22'
+    let hour_end = '22:21'
+    let ifo_end = 222;
+    let mgo_end = 440;
+    let totalBunkeringIFO = 0;
+    let totalBunkeringMGO = 0;
+
+    let totalConsumptIFO = 0;
+    let totalConsumptMGO = 0;
+
+
+
+    let colorYellowTransgas = 'FFCD06';
+    // Variables de colores-
+    let blueHard = '001556'
+    let blueMedium = '09155694'
+    let blueLow = 'b6c2ff94';
+
+
+    let blueHard1 = '375f9a'
+    let blueHard2 = '0040d8'
+    let blueHard3 = '001556'
+
+    let greenHard = '091556'
+    let greenMedium = ''
+    let greenLow = 'b6c2ff94';
+
+    let black = '000'
+    let white = 'ffffff';
+
+    // Variables de colores-
+    let grisFuerte = 'd4d4d4'
+    let grisMedio = 'ebe8e8'
+    let grisSuave = 'f3f3f3';
+
+    let redHard = '9a2929';
+    let redMedium = 'ffa4a4';
+    let redLow = 'ffd6d6';
+
+    let textIFOorVLSFOorLSFO = selectUser.isConsumptionIFO ? 'IFO' : selectUser.isConsumptionLSFO ? 'LSFO' : selectUser.isConsumptionVLSFO ? 'VLSFO' : 'LSFO';
+    //44
+    /* 
+       let positionRows = [posit, posit];
+        colum += 1;
+        positionRows = [posit, posit];
+        let positionColumns = [colum, colum + 4];
+        this.addStyleByColums(worksheet, positionRows, positionColumns, selectUser.name, 15, blueHard3, white, '')
+        this.addBorder(worksheet, posit, colum, '', blueHard3, '');
+    */
+
+    let positionRows = [posit, posit + 1];
+    let positionColumns = [colum, colum + 1];
+    this.addStyleByColums(worksheet, positionRows, positionColumns, 'SPEED', 10, colorYellowTransgas, blueHard3, '')
+
+    positionRows = [posit, posit];
+    positionColumns = [colum + 2, colum + 3];
+    this.addStyleByColums(worksheet, positionRows, positionColumns, 'CHARTER', 8, white, blueHard3, '')
+
+    positionColumns = [colum + 4, colum + 5];
+    this.addStyleByColums(worksheet, positionRows, positionColumns, 'PERFORMEND', 8, white, blueHard2, '')
+
+    posit += 1;
+    positionRows = [posit, posit];
+
+    // FULL Y ECO Charter SPEED
+    positionColumns = [colum + 2, colum + 2];
+    this.addStyleByColums(worksheet, positionRows, positionColumns, 'FULL', 8, white, blueHard3, '')
+    positionColumns = [colum + 3, colum + 3];
+    this.addStyleByColums(worksheet, positionRows, positionColumns, 'ECO', 8, white, blueHard3, '')
+
+    // FULL Y ECO Performan SPEED
+    positionColumns = [colum + 4, colum + 4];
+    this.addStyleByColums(worksheet, positionRows, positionColumns, 'FULL', 8, white, blueHard2, '')
+    positionColumns = [colum + 5, colum + 5];
+    this.addStyleByColums(worksheet, positionRows, positionColumns, 'ECO', 8, white, blueHard2, '')
+
+
+    posit += 1;
+    positionRows = [posit, posit];
+
+
+    // FULL Y ECO Charter SPEED IFO
+    positionColumns = [colum, colum + 1];
+    this.addStyleByColums(worksheet, positionRows, positionColumns, 'Ballast', 8, black, white, '');
+    positionColumns = [colum + 2, colum + 2];
+    this.addStyleByColums(worksheet, positionRows, positionColumns, selectUser.sailingBallastConsumptionIFO, 8, black, white, '');
+    positionColumns = [colum + 3, colum + 3];
+    this.addStyleByColums(worksheet, positionRows, positionColumns, selectUser.sailingEconomicConsumptionIFO, 8, black, white, '');
+
+    // FULL Y ECO Performan SPEED
+    positionColumns = [colum + 4, colum + 4];
+    this.addStyleByColums(worksheet, positionRows, positionColumns, 'E4', 8, black, white, '');
+    positionColumns = [colum + 5, colum + 5];
+    this.addStyleByColums(worksheet, positionRows, positionColumns, 'E5', 8, black, white, '')
+
+
+
+
+    posit += 1;
+    positionRows = [posit, posit];
+
+
+    // FULL Y ECO Charter SPEED IFO
+    positionColumns = [colum, colum + 1];
+    this.addStyleByColums(worksheet, positionRows, positionColumns, 'Laden', 8, black, white, '');
+    positionColumns = [colum + 2, colum + 2];
+    this.addStyleByColums(worksheet, positionRows, positionColumns, selectUser.contractSpeedSailingLadenIFO, 8, black, white, '');
+    positionColumns = [colum + 3, colum + 3];
+    this.addStyleByColums(worksheet, positionRows, positionColumns, selectUser.sailingEconomicConsumptionIFO, 8, black, white, '');
+
+    // FULL Y ECO Performan SPEED
+    positionColumns = [colum + 4, colum + 4];
+    this.addStyleByColums(worksheet, positionRows, positionColumns, 'E4', 8, black, white, '');
+    positionColumns = [colum + 5, colum + 5];
+    this.addStyleByColums(worksheet, positionRows, positionColumns, 'E5', 8, black, white, '')
+
+
+
+
+
+
+
+
+
+
+
+    /* 
+        this.addBorder(worksheet, posit, colum, '', blueHard3, 'leftLowRorner');
+        this.addBorder(worksheet, posit, colum + 13, '', blueHard3, 'righLowRorner')
+        this.addBorder(worksheet, posit - 3, colum + 5, '', blueHard3, 'top')
+        this.addBorder(worksheet, posit - 3, colum + 8, '', blueHard3, 'top')
+     */
     return posit;
   }
 }
