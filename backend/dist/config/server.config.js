665012be5aa4f8c9e0c66fb3498cf1ceb8068ef4
CristianPumaES6 <cristian.puma.es6@gmail.com>
2021-10-01 21:59:13 -0500

    ok

--- backend/dist/config/server.config.js
+++ backend/dist/config/server.config.js
@@ -4,7 +4,7 @@ exports.URL_Server = void 0;
 exports.URL_Server = {
     front: 'https://transgas.web.app',
     back: 'https://transgas.codev.site',
-    version: 'v1.9i',
+    version: 'v1.9h',
     puertoSocket: 4000,
 };
 //# sourceMappingURL=server.config.js.map
