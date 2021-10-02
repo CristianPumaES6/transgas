f8cb7ec045910bc7b5cc4d2a58a94530ba9435ba
CristianPumaES6 <cristian.puma.es6@gmail.com>
2021-10-01 21:57:43 -0500

    version h

--- backend/src/config/server.config.ts
+++ backend/src/config/server.config.ts
@@ -6,18 +6,18 @@
 };
  */
 
-/*  
+ 
 export const URL_Server = {
     front: 'https://transgas.web.app',
     back: 'https://transgas.codev.site',
-    version: 'v1.9i',
+    version: 'v1.9h',
     puertoSocket: 4000,
-}; */
+};
  
-
+/* 
 export const URL_Server = {
     front: 'http://localhost:3001',
     back: 'http://localhost:3000',
-    version: 'v1.9i',
+    version: 'v1.9h',
     puertoSocket: 4000,
-};
+}; */
