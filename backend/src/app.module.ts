663078074b28a49d8b5c6b1f5d399f71e26480aa
CristianPumaES6 <cristian.puma.es6@gmail.com>
2021-12-07 11:55:11 -0500

    modulo user

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -19,36 +19,34 @@ import { VoyagesModule } from './components/voyages/voyages.module';
 import { AppGateway } from './app.gateway';
 
 @Module({
- // imports: [
- //   TypeOrmModule.forRoot({
- //     type: 'sqlite',
- //     database: join(SQLITE_PATH, 'dbTransgas.sqlite3'),
- //     entities: [join(__dirname, '**/**.entity{.ts,.js}')],
- //     synchronize: true,
- //   }),
- //   UsersModule,
- //   AuthModule,
- //   VoyagesModule,
- //   AppGateway,// por mientras queeste desactivado
- // ],
-
-
-
   imports: [
     TypeOrmModule.forRoot({
-      type: 'mssql',
-      host: 'localhost',
-      port: 1433,
-      username: 'sa',
-      password: 'Lab2021Code.',
-      database: 'test',
+      type: 'sqlite',
+      database: join(SQLITE_PATH, 'dbTransgas.sqlite3'),
       entities: [join(__dirname, '**/**.entity{.ts,.js}')],
       synchronize: true,
-      options: {
-        encrypt: false,
-        enableArithAbort: true,
-      },
-    }),
+    }),  
+
+
+//
+//  imports: [
+//    TypeOrmModule.forRoot({
+//      type: 'mssql',
+//      host: 'localhost',
+//      port: 1433,
+//      username: 'sa',
+//      password: 'Lab2021Code.',
+//      database: 'prueba',
+//      entities: [join(__dirname, '**/**.entity{.ts,.js}')],
+//      synchronize: true,
+//      options: {
+//        encrypt: false,
+//        enableArithAbort: true,
+//      },
+//      extra:{
+//        trustServerCertificate: true,
+//      }
+//    }),
     UsersModule,
     AuthModule,
     VoyagesModule,
