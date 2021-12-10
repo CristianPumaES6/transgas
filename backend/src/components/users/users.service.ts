984d7ccf092ac06685eda85007826d8b8a8f37a9
CristianPumaES6 <cristian.puma.es6@gmail.com>
2021-12-10 12:47:31 -0500

    Modulo de suuarop

--- backend/src/components/users/users.service.ts
+++ backend/src/components/users/users.service.ts
@@ -26,40 +26,73 @@ export class UsersService {
     ) { }
 
     async Get(id: number): Promise<UserEntity> {
+        return DummyPromise().then(
+            result => {
 
-        // Hacemos una busqueda por id
-        return await this.userRepository.findOne({
-            where: {
-                id: id,
-                status: Not(false)
+                if (URL_Server.bd === 'MSSQL') {
+
+                    return this.userRepository.query(
+                        `SP_BuscarUsuarioPorId @userId= ${id}`
+                    )
+
+                } else {
+
+                    return this.userRepository.find({
+                        where: {
+                            id: id,
+                            status: Not(false)
+                        }
+                    });
+                }
+
+
             }
-        }).then(
+        ).then(
             resultFind => {
-                // Validamos si encontro al usuario.
-                if (!resultFind) throw new Error('user_does_not_exist');
+
+                if (!resultFind && resultFind.length == 0) throw 'NO_REGISTER'
+
+                let usuario: UserEntity = resultFind[0];
+
                 // Vaciamos el campo del password.
-                resultFind.password = null;
+                usuario.password = null;
                 // retornamos el objeto.
-                return resultFind;
+                return usuario;
             });
 
     }
 
     async Gets(user: UserEntity): Promise<UserEntity[]> {
+        return DummyPromise().then(
+            result => {
 
-        // Hacemos where por todos los campos de la entidad
-        return await this.userRepository.find({
-            where: [
-                // name && surname && nick && email
-                {
-                    id: (user.id || Like('%' + '%')),
-                    nick: Like('%' + (user.nick || '') + '%'),
-                    name: Like('%' + (user.name || '') + '%'),
-                    role: Like('%' + (user.role || '') + '%'),
-                    status: Not(false)
+                if (URL_Server.bd === 'MSSQL') {
+
+                    return this.userRepository.query(
+                        `EXEC SP_BuscarUsuariosByFilter @userId =0,@nick = '${user.nick || ''}',@name = '${user.name || ''}',@role= '${user.role || ''}'
+                    `
+                    );
+
+                } else {
+
+                    return this.userRepository.find({
+                        where: [
+                            // name && surname && nick && email
+                            {
+                                id: (user.id || Like('%' + '%')),
+                                nick: Like('%' + (user.nick || '') + '%'),
+                                name: Like('%' + (user.name || '') + '%'),
+                                role: Like('%' + (user.role || '') + '%'),
+                                status: Not(false)
+                            }
+                        ]
+                    });
+
                 }
-            ]
-        }).then(
+
+
+            }
+        ).then(
             (result: UserEntity[]) => {
 
                 // Recorremos y borramos el password.
@@ -71,50 +104,161 @@ export class UsersService {
                 // No lo validamos por que puede llegar vacio.
                 return result;
             }
+        ).catch(
+            error => {
+                throw 'ERROR MIGRACION SQLSERVER.';
+            }
         )
     }
 
     async CreateUserNickUnique(user: UserEntity): Promise<UserEntity> {
 
         // buscamos si el nick o email ya esta en uso.
-        return await this.userRepository.findOne({
-            where: [
-                // hacemos un where donde buscamos por nick o email.
-                {
-                    nick: user.nick
+        return DummyPromise().then(
+            result => {
+                if (URL_Server.bd === 'MSSQL') {
+
+                    return this.userRepository.query(
+                        `
+                        EXEC SP_BuscarUsuariosByFilter @userId =0,@nick = '${user.nick || ''}',@name = '${user.name || ''}',@role= '${user.role || ''}'
+                    `
+                    );
+
+                } else {
+                    return this.userRepository.find({
+                        where: [
+                            // hacemos un where donde buscamos por nick o email.
+                            {
+                                nick: user.nick
+                            }
+                        ]
+                    })
                 }
-            ]
-        }).then((result) => {
-            // validamos si exite un resultado.
-            if (result) {
-                // Si existe, generamos un error para no continual.
-                throw 'REPEAT_NICK';
             }
+        ).then(
+            resultFind => {
 
-            // encriptamos el password.
-            return bcrypt.hash(user.password, ROUNDS_BCRYPT);
+                if (!resultFind && resultFind.length > 0) throw 'REPEAT_NICK'
 
-        }).then(password => {
-            // le asignamos el password encriptado al objeto
-            user.password = password;
 
-            // Eliminamos el user id
-            delete user.id;
-            user.years = JSON.stringify(user.years);
-            // procedemos hacer el save.
-            return this.userRepository.save(user);
+                // encriptamos el password.
+                return bcrypt.hash(user.password, ROUNDS_BCRYPT);
 
-        }).then(
-            (resultSave: UserEntity) => {
-                // Validamos si encontro al usuario.
-                if (!resultSave) throw new Error('ERROR_USER_SERVICE_CREATE_USER_SAVE_USER');
+            }).then(password => {
+                // le asignamos el password encriptado al objeto
+                user.password = password;
 
-                // Borramos el password.
-                resultSave.password = '';
+                // Eliminamos el user id
+                delete user.id;
+                user.years = JSON.stringify(user.years);
+                // procedemos hacer el save.
 
-                return resultSave;
-            }
-        );
+                if (URL_Server.bd === 'MSSQL') {
+
+                    return this.userRepository.query(
+                        `
+                                        
+                    EXEC SP_CreateNewUser
+                    @nick ='${user.nick || ''}'
+                    ,@name ='${user.name || ''}'
+                    ,@filename ='${user.filename || ''}'
+                    ,@password ='${user.password || ''}'
+                    ,@language ='${user.language || ''}'
+                    ,@role ='${user.role || ''}'
+                    ,@years  ='[]'
+                    ,@minSpeed  = ${user.minSpeed || 0}
+                    ,@maxSpeed  = ${user.maxSpeed || 0}
+                    ,@isConsumptionIFO  = ${user.isConsumptionIFO || 0}
+                    ,@isConsumptionLSFO = ${user.isConsumptionLSFO || 0}
+                    ,@isConsumptionVLSFO  = ${user.isConsumptionVLSFO || 0}
+                    ,@isConsumptionMGO   = ${user.isConsumptionMGO || 0}
+                    ,@maxIFOConsumption   = ${user.maxIFOConsumption || 0}
+                    ,@maxMGOConsumption   = ${user.maxMGOConsumption || 0}
+                    ,@minIFOConsumption   = ${user.minIFOConsumption || 0}
+                    ,@minMGOConsumption   = ${user.minMGOConsumption || 0}
+                    ,@isMEMGO   = ${user.isMEMGO || 0}
+                    ,@isAEMGO   = ${user.isAEMGO || 0}
+                    ,@isBoilerMGO   = ${user.isBoilerMGO || 0}
+                    ,@isIGMGO   = ${user.isIGMGO || 0}
+                    ,@isPowerPMGO   = ${user.isPowerPMGO || 0}
+                    ,@isOtherMGO   = ${user.isOtherMGO || 0}
+                    ,@isMEIFO   = ${user.isMEIFO || 0}
+                    ,@isAEIFO   = ${user.isAEIFO || 0}
+                    ,@isBoilerIFO   = ${user.isBoilerIFO || 0}
+                    ,@isOtherIFO   = ${user.isOtherIFO || 0}
+                    ,@contractSpeedSailingBallastMGO   = ${user.contractSpeedSailingBallastMGO || 0}
+                    ,@contractSpeedSailingLadenMGO   = ${user.contractSpeedSailingLadenMGO || 0}
+                    ,@contractSpeedSailingEconomicalMGO   = ${user.contractSpeedSailingEconomicalMGO || 0}
+                    ,@loadingConsumptionMGO   = ${user.loadingConsumptionMGO || 0}
+                    ,@dischargeConsumptionMGO   = ${user.dischargeConsumptionMGO || 0}
+                    ,@sailingBallastConsumptionMGO   = ${user.sailingBallastConsumptionMGO || 0}
+                    ,@sailingLoadConsumptionMGO   = ${user.sailingLoadConsumptionMGO || 0}
+                    ,@sailingEconomicConsumptionMGO   = ${user.sailingEconomicConsumptionMGO || 0}
+                    ,@anchoredConsumptionMGO   = ${user.anchoredConsumptionMGO || 0}
+                    ,@maneuverConsumptionMGO   = ${user.maneuverConsumptionMGO || 0}
+                    ,@otherConsumptionMGO   = ${user.otherConsumptionMGO || 0}
+                    ,@contractSpeedSailingBallastIFO   = ${user.contractSpeedSailingBallastIFO || 0}
+                    ,@contractSpeedSailingLadenIFO   = ${user.contractSpeedSailingLadenIFO || 0}
+                    ,@contractSpeedSailingEconomicalIFO   = ${user.contractSpeedSailingEconomicalIFO || 0}
+                    ,@loadingConsumptionIFO   = ${user.loadingConsumptionIFO || 0}
+                    ,@dischargeConsumptionIFO   = ${user.dischargeConsumptionIFO || 0}
+                    ,@sailingBallastConsumptionIFO   = ${user.sailingBallastConsumptionIFO || 0}
+                    ,@sailingLoadConsumptionIFO   = ${user.sailingLoadConsumptionIFO || 0}
+                    ,@sailingEconomicConsumptionIFO   = ${user.sailingEconomicConsumptionIFO || 0}
+                    ,@anchoredConsumptionIFO   = ${user.anchoredConsumptionIFO || 0}
+                    ,@maneuverConsumptionIFO   = ${user.maneuverConsumptionIFO || 0}
+                    ,@otherConsumptionIFO   = ${user.otherConsumptionIFO || 0}
+                    ,@isDisplayLSFOConsumption   = ${user.isDisplayLSFOConsumption || 0}
+                    ,@isDisplayMGOConsumption   = ${user.isDisplayMGOConsumption || 0}
+                    ,@isDisplayAverageSpeed   = ${user.isDisplayAverageSpeed || 0}
+                    ,@isDisplayDataMGO   = ${user.isDisplayDataMGO || 0}
+                    ,@isDisplayDataLSFO   = ${user.isDisplayDataLSFO || 0}
+                    ,@isDisplayVesselPerformanceLSFO   = ${user.isDisplayVesselPerformanceLSFO || 0}
+                    ,@isDisplayVesselPerformanceMGO   = ${user.isDisplayVesselPerformanceMGO || 0}
+                    ,@consumptionEquipmentME_MGO   = ${user.consumptionEquipmentME_MGO || 0}
+                    ,@consumptionEquipmentAE_MGO   = ${user.consumptionEquipmentAE_MGO || 0}
+                    ,@consumptionEquipmentBOILER_MGO   = ${user.consumptionEquipmentBOILER_MGO || 0}
+                    ,@consumptionEquipmentIG_MGO   = ${user.consumptionEquipmentIG_MGO || 0}
+                    ,@consumptionEquipmentPP_MGO   = ${user.consumptionEquipmentPP_MGO || 0}
+                    ,@consumptionEquipmentOther_MGO   = ${user.consumptionEquipmentOther_MGO || 0}
+                    ,@consumptionEquipmentME_IFO   = ${user.consumptionEquipmentME_IFO || 0}
+                    ,@consumptionEquipmentAE_IFO   = ${user.consumptionEquipmentAE_IFO || 0}
+                    ,@consumptionEquipmentBOILER_IFO   = ${user.consumptionEquipmentBOILER_IFO || 0}
+                    ,@consumptionEquipmentOther_IFO   = ${user.consumptionEquipmentOther_IFO || 0}
+                    ,@userIdCreated   = ${user.userIdCreated || 0}
+                    ,@dateCreated   = '${user.dateCreated || ''}'
+                    ,@userIdUpdated   = ${user.userIdUpdated || 0}
+                    ,@dateUpdated   = '${user.dateUpdated || ''}'
+                    ,@status   = ${user.status || 0}
+
+`
+                    );
+
+                } else {
+
+                    return this.userRepository.save(user);
+                }
+
+            }).then(
+                (resultSave: any) => {
+
+
+                    if (!resultSave) throw new Error('No se puedo registrar el viaje en la BD.');
+
+                    if (URL_Server.bd === 'MSSQL') {
+                        // MSSQL
+                        if (resultSave.length == 0) throw new Error('No se puedo registrar el viaje en la BD.');
+                        return resultSave[0];
+                    } else {
+                        // SLQITE
+                        return resultSave;
+                    }
+                }
+            ).catch(
+                error => {
+                    throw 'ERROR MIGRACION SQLSERVER.'
+                }
+            );
     }
 
     // Actualiza un usuario
@@ -184,7 +328,11 @@ export class UsersService {
 
             // Envio respuesta con el resultado recibido del ultimo paso
             return user;
-        });
+        }).catch(
+            error => {
+                throw 'ERROR MIGRACION SQLSERVER.'
+            }
+        );
     }
 
     // Elimina a un usuario por id
@@ -219,6 +367,10 @@ export class UsersService {
 
                 return user;
             }
+        ).catch(
+            error => {
+                throw 'ERROR MIGRACION SQLSERVER.'
+            }
         )
     }
 
@@ -229,12 +381,12 @@ export class UsersService {
 
 
                 if (URL_Server.bd === 'MSSQL') {
-                
+
                     return this.userRepository.query(`
                         EXEC SP_GetUserByNick
                             @nick = ${nick}
                     `)
-                //
+                    //
 
                 }
                 else {
@@ -250,8 +402,12 @@ export class UsersService {
 
             if (!resultUser || (resultUser && !resultUser.length)) throw new Error('user_was_not_found');
 
-            return resultUser [0];
-        });
+            return resultUser[0];
+        }).catch(
+            error => {
+                throw 'ERROR MIGRACION SQLSERVER.'
+            }
+        );
     }
 
     // Actualiza el filename del usuario ademas retorna el newfilename.
@@ -266,7 +422,11 @@ export class UsersService {
 
             // Envio respuesta con el resultado recibido del ultimo paso
             return urlImage;
-        });
+        }).catch(
+            error => {
+                throw 'ERROR MIGRACION SQLSERVER.'
+            }
+        );
     }
 
 }
