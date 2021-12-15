c122137007a7e862e32fda20dc31541ec73f7202
CristianPumaES6 <cristian.puma.es6@gmail.com>
2021-12-15 16:28:22 -0500

    regresamos a la version actual

--- backend/dist/config/server.config.js
+++ backend/dist/config/server.config.js
@@ -4,7 +4,7 @@ exports.URL_Server = void 0;
 exports.URL_Server = {
     front: 'https://transgas.web.app',
     back: 'https://transgas.codev.site',
-    version: 'v1.9P',
+    version: 'v1.9M',
     puertoSocket: 4000,
     bd: 'SQLITE',
 };
