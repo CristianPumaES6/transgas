194e46495df19ceef09353a8ca38648f495ac6cc
Cristian <cristian.puma.es6@gmail.com>
2022-01-28 23:52:25 -0500

    Format document

--- frontend/src/app/services/detecte-inactive-user.service.ts
+++ frontend/src/app/services/detecte-inactive-user.service.ts
@@ -12,54 +12,54 @@ export class DetecteInactiveUserService {
   definedInactivityPeriod = 10000;
 
   constructor(
-    private rendererFactory2: RendererFactory2,  
-    private  router: Router){
- 
-     this.renderer = this.rendererFactory2.createRenderer(null, null);
-      this.renderer.listen('document', 'mousemove', (evt) => {
-        this.lastInteraction = new Date();
-      });
-      this.renderer.listen('document', 'mousedown', (evt) => {
-        this.lastInteraction = new Date();
-      });
-      this.renderer.listen('document', 'keypress', (evt) => {
-        this.lastInteraction = new Date();
-      });
-      this.renderer.listen('document', 'DOMMouseScroll', (evt) => {
-        this.lastInteraction = new Date();
-      });
-      this.renderer.listen('document', 'mousewheel', (evt) => {
-        this.lastInteraction = new Date();
-      });
-      this.renderer.listen('document', 'touchmove', (evt) => {
-        this.lastInteraction = new Date();
-      });
-      this.renderer.listen('document', 'MSPointerMove', (evt) => {
-        this.lastInteraction = new Date();
-      });
-      // Subscribing here for demo only
-      this.idlePoll().subscribe();
-    }
-  
-    idlePoll() {
-      return interval(3000)
-        .pipe(
-          tap(() => console.log('here', new Date().getTime() - this.lastInteraction.getTime())),
-          takeWhile(() => {
-            if (
-              (new Date().getTime() - this.lastInteraction.getTime()) > this.definedInactivityPeriod) {
-                console.log('----------------------------')
-                console.log('----------------------------')
-                console.log('---------------PASO EL TIMEPO-------------')
-                console.log('----------------------------')
-                console.log('----------------------------')
-                console.log('----------------------------')
-            }
-            return (new Date().getTime() - this.lastInteraction.getTime()) < this.definedInactivityPeriod;
-          })
-        );
-     }
+    private rendererFactory2: RendererFactory2,
+    private router: Router) {
+    // esc
+    this.renderer = this.rendererFactory2.createRenderer(null, null);
+    this.renderer.listen('document', 'mousemove', (evt) => {
+      this.lastInteraction = new Date();
+    });
+    this.renderer.listen('document', 'mousedown', (evt) => {
+      this.lastInteraction = new Date();
+    });
+    this.renderer.listen('document', 'keypress', (evt) => {
+      this.lastInteraction = new Date();
+    });
+    this.renderer.listen('document', 'DOMMouseScroll', (evt) => {
+      this.lastInteraction = new Date();
+    });
+    this.renderer.listen('document', 'mousewheel', (evt) => {
+      this.lastInteraction = new Date();
+    });
+    this.renderer.listen('document', 'touchmove', (evt) => {
+      this.lastInteraction = new Date();
+    });
+    this.renderer.listen('document', 'MSPointerMove', (evt) => {
+      this.lastInteraction = new Date();
+    });
+    // Subscribing here for demo only
+    this.idlePoll().subscribe();
+  }
+
+  idlePoll() {
+    return interval(3000)
+      .pipe(
+        tap(() => console.log('here', new Date().getTime() - this.lastInteraction.getTime())),
+        takeWhile(() => {
+          if (
+            (new Date().getTime() - this.lastInteraction.getTime()) > this.definedInactivityPeriod) {
+            console.log('----------------------------')
+            console.log('----------------------------')
+            console.log('---------------PASO EL TIMEPO-------------')
+            console.log('----------------------------')
+            console.log('----------------------------')
+            console.log('----------------------------')
+          }
+          return (new Date().getTime() - this.lastInteraction.getTime()) < this.definedInactivityPeriod;
+        })
+      );
+  }
 
 
- 
+
 }
