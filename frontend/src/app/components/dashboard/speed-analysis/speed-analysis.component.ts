54ccfd32fe99bc61ff18f0cc1ce20af87a9e9172
Cristian <cristian.puma.es6@gmail.com>
2022-02-11 08:54:04 -0500

    velocidad

--- frontend/src/app/components/dashboard/speed-analysis/speed-analysis.component.ts
+++ frontend/src/app/components/dashboard/speed-analysis/speed-analysis.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {  ChartData,registerables  } from 'chart.js';
+import { getRelativePosition } from 'chart.js/helpers';
+import Chart from 'chart.js/auto';
+
 
 @Component({
   selector: 'app-speed-analysis',
@@ -7,9 +11,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SpeedAnalysisComponent implements OnInit {
 
-  constructor() { }
+  constructor() { 
+  }
 
   ngOnInit(): void {
+
+    let canvaMyChart: any = document.getElementById('myChart');
+    // Convertimos el canvaLineIfo en 2d
+    let ctxMyChart = canvaMyChart.getContext('2d');
+    
+    const myChart = new Chart(
+      ctxMyChart,
+       {
+        type: 'line',
+        data:  {
+          datasets: [{
+            data: [{x: 10, y: 20}, {x: 15, y: null}, {x: 20, y: 10}]
+          }]
+        },
+        options: {
+          onClick: (e) => {
+            const canvasPosition = getRelativePosition(e, myChart);
+      
+            // Substitute the appropriate scale IDs
+            const dataX = myChart.scales.x.getValueForPixel(canvasPosition.x);
+            const dataY = myChart.scales.y.getValueForPixel(canvasPosition.y);
+          }
+        }
+      }
+    );
   }
 
 }
