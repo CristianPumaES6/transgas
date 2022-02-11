90077bc423dd0454e4d9fdbd03abaa0f26b04249
Cristian <cristian.puma.es6@gmail.com>
2022-02-11 09:09:02 -0500

    ok

--- frontend/src/app/components/dashboard/speed-analysis/speed-analysis.component.ts
+++ frontend/src/app/components/dashboard/speed-analysis/speed-analysis.component.ts
@@ -11,35 +11,184 @@ import Chart from 'chart.js/auto';
 })
 export class SpeedAnalysisComponent implements OnInit {
 
+  // Configuracion del SPEED
+  public configLineaSPEED: any; // configuracion del elemento
+  public chartLineSPEED: Chart; // LINEA
+  public dataSPEED: Chart.ChartPoint[] = []; // Data
+  
+  public xLabelReport: any[] = [];
+
   constructor() { 
   }
 
   ngOnInit(): void {
 
-    let canvaMyChart: any = document.getElementById('myChart');
-    // Convertimos el canvaLineIfo en 2d
-    let ctxMyChart = canvaMyChart.getContext('2d');
+
     
-    const myChart = new Chart(
-      ctxMyChart,
-       {
-        type: 'line',
-        data:  {
-          datasets: [{
-            data: [{x: 10, y: 20}, {x: 15, y: null}, {x: 20, y: 10}]
-          }]
+    // Agregamos la configuracion del ChartSpeed.
+    this.configLineaSPEED = {
+      type: 'line',
+      data: {
+        labels: [], // Lo pongo vacio por que en el update se colocara el valor.
+        datasets: [{
+          label: 'AVERAGE SPEED',
+          backgroundColor: 'rgb(255,205,6)',
+          borderColor: 'rgb(255,205,6)',
+          data: [], // Lo pongo vacio por que en el update se colocara el valor.
+          fill: false,
+        }]
+      },
+      options: { // Otras opciones dentro del Chart
+        onClick: (event, activeElement) => { 
+          // REVISAR ESTO, Aqui se ejecuta la data que se muestra al dar click a los puntos dentro del chart.
+
         },
-        options: {
-          onClick: (e) => {
-            const canvasPosition = getRelativePosition(e, myChart);
-      
-            // Substitute the appropriate scale IDs
-            const dataX = myChart.scales.x.getValueForPixel(canvasPosition.x);
-            const dataY = myChart.scales.y.getValueForPixel(canvasPosition.y);
+        legend: { // La leyenda es el texto que esta arriva del cuadro.
+          display: true,
+          onClick: (event, legendItem) => {
+            console.log('onClick:' + legendItem.text);
+          },
+          labels: {
+            fontColor: 'rgb(255,255,255)', // Color de la leyenda.
+            fontStyle: 'bold', // Tipo de texto de la leyenda.
+          }
+        },
+        // Habilitamos la opcion para que sea responsive
+        maintainAspectRatio: false,
+        tooltips: {}, // Lo pongo vacio por que en// Lo pongo vacio por que en el update se colocara el valor.
+        scales: {},// Lo pongo vacio por que en el update se colocara el valor.
+        hover: {
+          onHover: function (e: MouseEvent) {
+            // puntos GetElementAtaEvent
+            var point = this.getElementAtEvent(e);
+
+            // event targer.
+            let eventTarget = e.target as HTMLCanvasElement;
+            ///home/kali/.vscode/extensions/ms-vscode.vscode-typescript-next-4.3.20210505/node_modules/typescript/lib/lib.dom.d.ts
+            if (point.length) {
+              eventTarget.style.cursor = 'pointer';// Aqui se esta modificando el TypeScript.
+            } else {
+              eventTarget.style.cursor = 'default';
+            }
           }
         }
+      },
+      lineaMax: 0 // Lo pongo cero por que en el update se colocara el valor.
+    };
+
+    
+        // Encapculamos el elemento del dom.
+        let canvaLineSPEED: any = document.getElementById('myChart');
+        // Convertimos el canvaLineIfo en 2d
+        let ctxLineSPEED: any = canvaLineSPEED.getContext('2d');
+    
+        this.chartLineSPEED = new Chart(ctxLineSPEED, this.configLineaSPEED);
+  }
+
+  
+  private UpdateLineSPEED(): boolean {
+    console.log('UpdateLineSPEED()');
+
+
+    // Actualizamos los labels
+    this.configLineaSPEED.data.labels = this.xLabelReport;
+
+    // Actualizamos la dataSPEED
+    this.configLineaSPEED.data.datasets[0].data = this.dataSPEED;
+
+    // Vaciamos la configuracion de las lines SPEED
+    // La linea es el campo que agregamos en el plugin.
+    this.configLineaSPEED.options.lines = [];
+
+
+
+
+
+    // Si ninguna actividad a sido seleccionada, agregamos la linea maxima segun configuracion.
+   
+
+      // Si el consumo maximo es mayor a 0 lo pintamos si no, no hace falta.
+     // if (this.selectUser.maxSpeed > 0) {
+       /*  this.configLineaSPEED.options.lines.push({
+          type: 'horizontal',
+          y: this.selectUser.maxSpeed,
+          color: 'red',
+          label: ''
+        }); */
+     // };
+/* 
+      if (this.selectUser.minSpeed > 0) {
+        this.configLineaSPEED.options.lines.push({
+          type: 'horizontal',
+          y: this.selectUser.minSpeed,
+          color: '#39FF14',
+          label: ''
+        });
+         
+      }*/
+
+
+      // Esta linea maxima es para la scala del cuadro.
+     /*  if (this.configLineaSPEED.lineaMax < this.selectUser.maxSpeed) {
+        this.configLineaSPEED.lineaMax = this.selectUser.maxSpeed;
+      } */
+/* 
+    } else {
+      // AQUI RECORREMOS TODAS LAS ACTIVIDADES CON EL FIN DE EL CONSTRAR LA MAYOR LINEA MAXIMA.
+
+      let lineaMaxByActivity = 0;
+      this.frmCActivityPerformed.value.forEach(activity => {
+
+        let lineMax = 0;
+
+        if (activity === 'SAILING_IN_BALLAST') { lineMax = this.selectUser.contractSpeedSailingBallastIFO; }
+        else if (activity === 'SAILING_WITH_LADEN') { lineMax = this.selectUser.contractSpeedSailingLadenIFO; }
+        else if (activity === 'ECONOMICAL_NAVIGATION') { lineMax = this.selectUser.contractSpeedSailingEconomicalIFO; }
+
+        if (lineMax > lineaMaxByActivity) {
+          lineaMaxByActivity = lineMax;
+        }
+
+      });
+
+      // Verificamos que la mayor linea maxima de las actividades sea mayor a 0 para ponerlo.
+      if (lineaMaxByActivity > 0) {
+
+        this.configLineaSPEED.options.lines.push({
+          type: 'horizontal',
+          y: lineaMaxByActivity,
+          color: '#39FF14',
+          label: ''
+        });
       }
-    );
+
+ */
+      // Esta linea maxima es para la scala del cuadro.
+      /* if (this.configLineaSPEED.lineaMax < lineaMaxByActivity) {
+        this.configLineaSPEED.lineaMax = lineaMaxByActivity;
+      } 
+
+    }
+*/
+
+
+    // Configuracion Tooltips
+    // this.configLineaSPEED.options.tooltips = this.GetToolTipConfig('SPEED');
+
+
+
+/* 
+
+    if (this.configLineaSPEED.lineaMax < this.selectUser.maxSpeed) {
+      this.configLineaSPEED.lineaMax = this.selectUser.maxSpeed;
+    } */
+
+    // Agregamos la configuracion de las escalas.
+   /*  this.configLineaSPEED.options.scales = this.ConfigScales(this.xLabelReport, true, mathRound(this.configLineaSPEED.lineaMax, 0) + 2);
+ */
+    this.chartLineSPEED.update();
+
+    return false;
   }
 
 }
