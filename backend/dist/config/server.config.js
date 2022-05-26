1640da86ca3de5cc254cdef118a87b0faf0f4ebf
Cristian <cristian.puma.es6@gmail.com>
2022-05-26 04:50:14 -0500

    Ultimo update

--- backend/dist/config/server.config.js
+++ backend/dist/config/server.config.js
@@ -2,8 +2,8 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.URL_Server = void 0;
 exports.URL_Server = {
-    front: 'https://transgas.esteesel.club',
-    back: 'https://transgas.esteesel.club',
+    front: 'https://transgas.web.app',
+    back: 'https://transgas.lowcodetool.com',
     version: 'v1.9M',
     puertoSocket: 4000,
     bd: 'SQLITE',
