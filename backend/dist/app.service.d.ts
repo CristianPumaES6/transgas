069e80527f6daa39675cd453a1c1a2f9ce3e7c70
cristian <cristian.puma.es6@gmail.com>
2022-11-02 09:19:22 -0500

    delete

--- backend/dist/app.service.d.ts
+++
@@ -1,6 +0,0 @@
-import { AppGateway } from './app.gateway';
-export declare class AppService {
-    private gateway;
-    constructor(gateway: AppGateway);
-    EmitConnect(): boolean;
-}
--- backend/dist/assets/moment.assets.d.ts
+++
@@ -1,4 +0,0 @@
-export declare function GetDate(): any;
-export declare function ConvertMMDDYYYToYYYYMMDD(dateMMDDYYY: any): Date;
-export declare function FormatDateUTCToDateHour(dateUTC: any): string;
-export declare function FormatDateSumDays(dateUTC: any, subtractDays: number): string;
