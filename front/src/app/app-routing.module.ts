2476e60b8c162c2eb3afce8a1898188df30052f2
Cristian Angel Puma Villalva <cristian.puma.es6@gmail.com>
2023-02-23 06:29:57 -0500

    ok

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+// Components CRUD
+import { LogInComponent } from './components/log-in/log-in.component';
+import { IsLoginGuard } from './guard/is-login.guard';
 
-@NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
-})
+import { IsUpdateServerGuard } from './guard/is-update-server.guard';
+
+// Guard
+
+
+const routes: Routes = [
+  { path: '', component: LogInComponent, canActivate: [IsUpdateServerGuard,IsLoginGuard]},
+  { path: 'application', redirectTo: '/application', pathMatch: 'full' },
+  { path: 'application/**', redirectTo: '/application', pathMatch: 'full' },
+];
 export class AppRoutingModule { }
--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -3,16 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LogInComponent } from './components/log-in/log-in.component';
+import { GlobalModule } from './global.module';
+
+import { AuthGuardService } from './services/auth-guard.service';
+import { ApplicationModule } from './application/application.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LogInComponent
   ],
   imports: [
+    GlobalModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ApplicationModule,
   ],
-  providers: [],
+  providers: [AuthGuardService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
