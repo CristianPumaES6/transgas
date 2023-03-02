522f02180d6fcb9e0724e5e3f568bc48598e4fed
Cristian Angel Puma Villalva <cristian.puma.es6@gmail.com>
2023-03-02 15:12:25 -0500

    regresando serveer como antes

--- backend/src/config/server.config.ts
+++ backend/src/config/server.config.ts
@@ -12,13 +12,14 @@ export const URL_Server = {
  
 
 // SERVIDOR LINUX - FRONT Firebase
- export const URL_Server = {
+/*export const URL_Server = {
     front: 'https://transgas.web.app',
     back: 'https://transgas.lowcodetool.com',
     version: 'v1.9M',
     puertoSocket: 4000,
     bd: 'SQLITE', // MSSQL, MYSQL
 };  
+*/
  
 
 
@@ -33,10 +34,10 @@ export const URL_Server = {
 */
 
 // CONFIGURACION PARA DESARROLLO SERVER
-/* export const URL_Server = {
+ export const URL_Server = {
     front: 'https://transgas.esteesel.club',
     back: 'https://transgas.esteesel.club',
     version: 'v1.9M',
     puertoSocket: 4000,
     bd: 'SQLITE', // MSSQL,   SQLITE,   MYSQL
-};  */
+};  
