a24ece518f411b25e08748a285f34ba2f9f187b5
Cristian Angel Puma Villalva <cristian.puma.es6@gmail.com>
2023-03-29 22:35:40 -0500

    ok

--- backend/query_sql/obtener los reportes que pertenecen a un rango.sql
+++ backend/query_sql/obtener los reportes que pertenecen a un rango.sql
@@ -1,13 +1,30 @@
+
+ -- Obtine los reportes de que esten activos o no lo esten
+	SELECT daily_report.id, daily_report.date, daily_report.status 'REPORTE' , port.id,  port.status 'port2', voyage.id, voyage.status 'VOYAGE'
+	FROM
+		daily_report
+	INNER JOIN 
+		PORT on daily_report.portId = port.id 
+	INNER JOIN 
+		VOYAGE on port.voyageId = voyage.id  
+
+	where daily_report.userId =24
+
+		AND daily_report.date <= datetime("2022-06-29 14:30:00.000")
+
+		AND daily_report.date >= datetime("2021-12-31 14:30:00.000")
+	order by daily_report.date ASC; 
+
 -- retorna todos los que esten activos desde el viaje port 
-SELECT *
-FROM daily_report
-INNER JOIN PORT on daily_report.portId = port.id AND port.status =1
-INNER JOIN VOYAGE on port.voyageId = voyage.id AND voyage.status = 1
-where daily_report.status =1
-AND daily_report.userId = 10  
- ORDER by daily_report.id ASC;
- 
- 
+					SELECT *
+					FROM daily_report
+					INNER JOIN PORT on daily_report.portId = port.id AND port.status =1
+					INNER JOIN VOYAGE on port.voyageId = voyage.id AND voyage.status = 1
+					where daily_report.status =1
+					AND daily_report.userId = 10  
+					 ORDER by daily_report.date ASC;
+					 
+				 
  
  -- Obtine los reportes de que esten activos o no lo esten
  SELECT daily_report.date,port.status, voyage.status ,  daily_report.status  
@@ -37,7 +54,7 @@ where daily_report.userId = 10
 	 )
  )
 	
-	 
+	 ;
  
  
  -- Busca los viajes
@@ -61,6 +78,15 @@ where daily_report.userId = 10
 		 from voyage
 		 where  voyage.id >=84 and  voyage.userId = 10
  
+;
+
+
+-- eliminar todos los reportes
+
+
+ update  daily_report
+ SET status=0
+ where  userId = 10; 
 ;
 
 
