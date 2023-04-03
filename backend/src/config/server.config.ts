f3016136aeb26ad9f7c7cb7f19a1d640df569fbc
Cristian Angel Puma Villalva <cristian.puma.es6@gmail.com>
2023-04-03 09:03:04 -0500

    local host

--- backend/src/config/server.config.ts
+++ backend/src/config/server.config.ts
@@ -7,10 +7,9 @@ export const URL_Server = {
     puertoSocket: 4000,
     bd: 'MSSQL', // MSSQL, MYSQL
 };
- */
+*/
 
- 
-
+/*
 // SERVIDOR LINUX - FRONT Firebase
 export const URL_Server = {
     front: 'https://transgas.web.app',
@@ -19,25 +18,26 @@ export const URL_Server = {
     puertoSocket: 4000,
     bd: 'SQLITE', // MSSQL, MYSQL
 }; 
-
- 
+*/
 
 
 // CONFIGURACION PARA DESARROLLO local
-/* export const URL_Server = {
+ export const URL_Server = {
     front: 'http://localhost:3001',
     back: 'http://localhost:3000',
     version: 'v1.9M',
     puertoSocket: 4000,
     bd: 'SQLITE', // MSSQL,   SQLITE,   MYSQL
-}; */
+};
 
 
 // CONFIGURACION PARA DESARROLLO SERVER
-/* export const URL_Server = {
+/*
+export const URL_Server = {
     front: 'https://transgas.esteesel.club',
     back: 'https://transgas.esteesel.club',
     version: 'v1.9M',
     puertoSocket: 4000,
     bd: 'SQLITE', // MSSQL,   SQLITE,   MYSQL
-}; */
+};
+*/
