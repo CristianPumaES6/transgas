df949e96c1b080b2bf4c2fb24c66b8e9db093c2a
Cristian Angel Puma Villalva <cristian.puma.es6@gmail.com>
2024-09-02 14:11:38 -0500

    transgas2

--- backend/src/config/server.config.ts
+++ backend/src/config/server.config.ts
@@ -44,7 +44,7 @@ export const URL_Server = {
 
 
 // CONFIGURACION PARA DESARROLLO SERVER
-export const URL_Server = {
+/*export const URL_Server = {
     front: 'https://esteesel.club',
     back: 'https://esteesel.club',
     version: 'v2',
@@ -53,23 +53,22 @@ export const URL_Server = {
     bd: 'SQLITE', // MSSQL, MYSQL, MSSQL2
     emailNotification: 'transgasshippinglines@gmail.com',
     passwordNotification: 'getlsmyuiluiwktq'
-};
+};*/
 
 
 
 
 // SERVIDOR LINUX - FRONT Firebase
-/* export const URL_Server = {
+ export const URL_Server = {
     front: 'https://transgas2.esteesel.club',
     back: 'https://transgas2.esteesel.club',
     version: 'v2',
-    puertoSocket: 4000,
-    puertoBackend: 3000,
+    puertoSocket: 4002,
+    puertoBackend: 3002,
     bd: 'SQLITE', // MSSQL, MYSQL
     emailNotification: 'transgasshippinglines@gmail.com',
     passwordNotification: 'getlsmyuiluiwktq'
-}; */
-
+}; 
 
 /*
 // SERVIDOR LINUX - FRONT LINUX
