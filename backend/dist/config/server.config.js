1e74c027770017645835d417231ff5ea82ea3c98
Cristian Angel Puma Villalva <cristian.puma.es6@gmail.com>
2025-01-23 15:04:17 -0500

    update

--- backend/dist/config/server.config.js
+++ backend/dist/config/server.config.js
@@ -2,11 +2,11 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.URL_Server = void 0;
 exports.URL_Server = {
-    front: 'https://nautic.solutions',
-    back: 'https://nautic.solutions',
-    version: 'v2',
-    puertoSocket: 4000,
-    puertoBackend: 3000,
+    front: 'https://dev-transgas.nautic.solutions',
+    back: 'https://dev-transgas.nautic.solutions',
+    version: 'v3',
+    puertoSocket: 2100,
+    puertoBackend: 2000,
     bd: 'MSSQL2',
     emailNotification: 'transgasshippinglines@gmail.com',
     passwordNotification: 'getlsmyuiluiwktq'
