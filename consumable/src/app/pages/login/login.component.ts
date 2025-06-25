9cf7bbdd0845907813d04e3bb8d50ba94570ee3e
Cristian Angel Puma Villalva <cristian.puma.es6@gmail.com>
2025-06-25 16:39:10 -0500

    ng generate service core/services/auth --skip-tests

--- consumable/src/app/pages/login/login.component.ts
+++ consumable/src/app/pages/login/login.component.ts
@@ -1,11 +1,41 @@
 import { Component } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  standalone: true,
+  imports: [
+    CommonModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatCardModule,
+    MatButtonModule
+  ],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrls: ['./login.component.scss']
 })
-export class LoginComponent {
 
+export class LoginComponent {
+  email: string = '';
+  password: string = '';
+
+  onLogin(form: NgForm) {
+    if (form.invalid) {
+      return;
+    }
+
+    console.log('✅ Login correcto:', this.email, this.password);
+    // lógica real aquí
+  }
+
+  onForgotPassword() {
+    console.log('👉 Redirigir a recuperación de contraseña');
+    // Aquí puedes redirigir a otra ruta, abrir modal, etc.
+  }
 }
